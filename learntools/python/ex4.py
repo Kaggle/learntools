30fe8404ba4d48c8a1e8faa901baf2f93c85bd05
Dan Becker <dansbecker@gmail.com>
2018-06-13 13:14:48 -0600

    Fix ex4.py syntax errors

--- learntools/python/ex4.py
+++ learntools/python/ex4.py
@@ -1,6 +1,7 @@
 from learntools.python.utils import bind_exercises, format_args
 from learntools.python.problem import *
 from learntools.python.richtext import *
+CS = CodeSolution
 
 # Complete fn that takes a list and returns the second element
 
@@ -9,8 +10,8 @@ class SelectSecondItem(FunctionProblem):
     _var = 'select_second'
 
     _test_cases = [
-            ([1,2, 3], 2),
-            ([[1],[2],[4]], [2]]),
+            ([1, 2, 3], 2),
+            ([[1], [2], [4]], [2]),
             (list(range(10)), 1),
     ]
 
@@ -26,7 +27,7 @@ class SecondItemLastSublist(FunctionProblem):
     _test_cases = [
             ([[0, 1, 2]], 1),
             ([[1,2], [[[1], [2]]]], [2]),
-            ([[1], [2,3], [4,5,6]]), 5),
+            ([[1], [2,3], [4,5,6]], 5),
     ]
 
     _hint = ("The last item in a list a can be selected with a[-1]."
@@ -54,16 +55,16 @@ class SwapFirstLast(FunctionProblem):
                  [[1],[2]],
                  [1,2,3,4,5]
                 )
-        sol_fn = lambda x: x[0], x[-1] = x[-1], x[0]
+        def sol_fn(x): x[0], x[-1] = x[-1], x[0]
         for l in lists:
             copy_for_soln_fn = l.copy()
             copy_for_user_fn = l.copy()
             sol_fn(copy_for_soln_fn) # create desired output for comparison
             fn_output = fn(copy_used_by_user_fn) # also applies swap in this line
             assert(type(fn_output) == NoneType), "Your function should not return anything"
-            assert copy_for_user_fn == copy_for_soln_fn,
-                "Failed on list " + l +
-                ". Expected " + copy_for_soln_fn  +
+            assert copy_for_user_fn == copy_for_soln_fn, \
+                "Failed on list " + l + \
+                ". Expected " + copy_for_soln_fn  + \
                 ". Your function created " + copy_for_user_fn
 
 
