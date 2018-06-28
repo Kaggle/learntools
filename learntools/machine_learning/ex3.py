a6a5c051f0b3b1b155af2add13a1fa56b5a80cf2
Dan Becker <dansbecker@gmail.com>
2018-06-27 18:34:32 -0600

    partial progress on ml/ex3.py

--- learntools/machine_learning/ex3.py
+++ learntools/machine_learning/ex3.py
@@ -8,35 +8,52 @@ from learntools.core.problem import *
 
 class SetTarget(VarCreationProblem):
     _var = 'y'
-    _hint = "Use the `pd.read_csv` function"
-    _solution = CS('home_data = pd.read_csv(iowa_file_path)')
+    _hint = "Use `print(home_data.columns)`. The column you want is at the end of the list. Use the dot notation to pull out this column from the DataFrame"
+    _solution = CS('y = home_data.SalePrice')
 
+    def check(self, targ):
+        assert isinstance(targ, pd.Series), ("`home_data` should be a Pandas Series with the actual data. Your current answer is a"
+                "  `{}`").format(type(targ),)
+        true_mean = 180921.19589041095
+        assert int(targ.mean()) == int(true_mean), ("You've selected the wrong data.")
+
+class ChoosePredictors(VarCreationProblem):
+    _vars = ['predictor_names']
+    _expected = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+    _hint = 'Create a list of strings with the specified names. Capitalization and spelling is important.'
+    _solution = CS(
+"""predictor_names = ["LotArea", "YearBuilt", "1stFlrSF", "2ndFlrSF", "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
+    """)
+
+class SelectPredictionData(VarCreationProblem):
+    _vars = ['X']
+    _hint = 'Set X equal to home data with the right set of predictors. Use the brackets notation.'
+    _solution = CS(
+"""# requires you have set predictor_names correctly
+X=home_data[predictor_names]""")
     def check(self, df):
-        assert isinstance(df, pd.DataFrame), ("`home_data` should be a DataFrame,"
+        assert isinstance(df, pd.DataFrame), ("`X` should be a DataFrame,"
                 " not `{}`").format(type(df),)
-        expected_shape = (1460, 81)
+        expected_shape = (1460, 7)
         assert df.shape == expected_shape, ("Expected {} rows and {} columns, but"
                 " got shape {}").format(expected_shape[0], expected_shape[1], df.shape)
 
-class HomeDescription(VarCreationProblem):
-    _vars = ['ave_lot_size', 'newest_home_age']
-    _expected = [10517, 8]
-    _hint = 'Run the describe command. Lot size is in the column called LotArea. Also look at YearBuilt'
-    _solution = CS(
-"""# using data read from home_data.describe()
-avg_lot_size = 10517
-newest_home_age = 8
+class SpecifyModel(VarCreationProblem):
+    _vars = ['iowa_model']
+    _hint = ['You will need to import DecisionTreeRegressorModel but you want need to supply arguments when calling it.]
+    def check(self, dtree):
+        from sklearn.tree import DecisionTreeRegressor
+        assert type(dtree) = sklearn.tree.tree.DecisionTreeRegressor, ("Expected type object of type DecisionTreeRegressor but got an object of type {}").format(type(dtree))
 
-    """)
-
-# Shorter syntax equivalent to above
-# from learntools.core.problem_factories import simple_problem
-# hd = simple_problem('HomeDescription', solution='idk', hint='This is a hint')\
-#        .with_expected(avg_lot_size=10516.828, newest_home_age=8)
+class FitModel():
 
 qvars = bind_exercises(globals(), [
     SetTarget,
-    HomeDescription,
+    ChoosePredictors,
+    SelectPredictionData,
+    SpecifyModel,
+    FitModel,
+    MakePredictions
     ],
     var_format='step_{n}',
     )
