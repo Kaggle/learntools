2f5c3ac82cf4b1deeb8f71e1dfb391cfbaff9591
Colin Morris <colin.morris2@gmail.com>
2018-06-28 10:50:19 -0400

    more commenting for some example solutions

--- learntools/python/solns/word_search.py
+++ learntools/python/solns/word_search.py
@@ -1,8 +1,14 @@
 def word_search(documents, keyword):
-    indices = []
+    # list to hold the indices of matching documents
+    indices = [] 
+    # Iterate through the indices (i) and elements (doc) of documents
     for i, doc in enumerate(documents):
+        # Split the string doc into a list of words (according to whitespace)
         tokens = doc.split()
+        # Make a transformed list where we 'normalize' each word to facilitate matching.
+        # Periods and commas are removed from the end of each word, and it's set to all lowercase.
         normalized = [token.rstrip('.,').lower() for token in tokens]
+        # Is there a match? If so, update the list of matching indices.
         if keyword.lower() in normalized:
             indices.append(i)
     return indices
