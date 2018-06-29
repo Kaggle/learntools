dfdb4375de9beae899c573aad9c6b001bd44b776
Dan Becker <dansbecker@gmail.com>
2018-06-29 06:52:32 -0600

    Combine some exercises for slightly more open workflow

--- learntools/machine_learning/ex3.py
+++ learntools/machine_learning/ex3.py
@@ -20,22 +20,15 @@ class SetTarget(CodingProblem):
         true_mean = 180921.19589041095
         assert int(targ.mean()) == int(true_mean), ("You've selected the wrong data.")
 
-class ChoosePredictors(EqualityCheckProblem):
-    _var = 'predictor_names'
-    _expected = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF',
-                 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-    _hint = ("Create a list of strings with the specified names. Capitalization "
-             "and spelling is important.")
-    _solution = CS(
-"""predictor_names = ["LotArea", "YearBuilt", "1stFlrSF", "2ndFlrSF",
-                      "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
-    """)
 
 class SelectPredictionData(CodingProblem):
     _var = 'X'
-    _hint = 'Set X equal to home data with the right set of predictors. Use the brackets notation.'
+    _hint = ("Capitalization and spelling are important when specifying variable names. "
+             "Use the brackets notation when data for X.")
     _solution = CS(
-"""# requires you have set predictor_names correctly
+"""predictor_names = ["LotArea", "YearBuilt", "1stFlrSF", "2ndFlrSF",
+                      "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
+
 X=home_data[predictor_names]""")
     def check(self, df):
         assert isinstance(df, pd.DataFrame), ("`X` should be a DataFrame,"
@@ -45,45 +38,39 @@ X=home_data[predictor_names]""")
                 " got shape {}").format(expected_shape[0], expected_shape[1], df.shape)
 
 
-class SpecifyModel(CodingProblem):
+class CreateModel(CodingProblem):
     _var = 'iowa_model'
-    _hint = ("You will need to import DecisionTreeRegressorModel but you want need "
-             "to supply arguments when calling it.")
+    _hint = ("Include random_state when specifying model. Data is specified when fitting it.")
     _solution = CS("""from sklearn.tree import DecisionTreeRegressor
-iowa_model = DecisionTreeRegressor(random_state=1)""")
+iowa_model = DecisionTreeRegressor(random_state=1)
+iowa_model.fit(X, y)""")
 
     def check(self, dtree):
+        # Not checking what they fit, because likely mistakes cause exceptions
+
         assert type(dtree) == sklearn.tree.tree.DecisionTreeRegressor, \
                     ("Expected type object of type DecisionTreeRegressor but got an "
                      "object of type {}").format(type(dtree))
-        assert dtree.random_state == 1
+        assert dtree.random_state is not None, "You forgot to set the random_state."
+        assert dtree.tree_, "You have not fit the model."
 
-class FitModel(CodingProblem):
-    _var = 'iowa_model'
-    _hint = 'The only arguments you need for the fit method have been stored as X and y'
-    _solution = CS("""iowa_model.fit(X, y)""")
-    def check(self, iowa_model):
-        # most mistakes in fitting will cause Exceptions. Accept anything for now
-        pass
+
 
 class MakePredictions(CodingProblem):
-    _vars = ['first_preds', 'iowa_model', 'X']
-    _hint = """Use iowa_model.predict with an argument holding the data to predict with.
-    Use head on the predictors rather than the predictions."""
-    _solution = 'iowa_model.predict(X.head())'
-    def check(self, preds):
-        ground_truth = iowa_model.predict(X.head())
-        preds_len = len(preds)
-        assert (preds_len == 5), ("Expected 5 predictions. Received {}. Did you use head()?").format(preds_len)
-        assert all(preds == ground_truth), ("Expected {} but got predictions {}").format(ground_truth, preds)
+    _vars = ['predictions', 'iowa_model', 'X']
+    _hint = """Use iowa_model.predict with an argument holding the data to predict with."""
+    _solution = 'iowa_model.predict(X)'
+    def check(self, predictions, iowa_model, X):
+        # This step is just checking that they can make predictions.
+        # If we want to check model is correct, do it in fitting step.
+        ground_truth = iowa_model.predict(X)
+        assert all(predictions == ground_truth), ("Expected {} but got predictions {}").format(ground_truth, preds)
 
 
 qvars = bind_exercises(globals(), [
     SetTarget,
-    ChoosePredictors,
     SelectPredictionData,
-    SpecifyModel,
-    FitModel,
+    CreateModel,
     MakePredictions
     ],
     var_format='step_{n}',
