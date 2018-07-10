31da9bab425a1cd99e3915f3c2601dd8c11efc25
Colin Morris <colin.morris2@gmail.com>
2018-07-10 18:11:04 -0400

    update slugs, scriptids, fix exercise imports

--- notebooks/partials/python/nbconvert_config.py
+++ notebooks/partials/python/nbconvert_config.py
@@ -1,94 +1,82 @@
-TODO = 'TODO'
 lessons_meta = [
-        # TODO: provisional scriptids, slugs etc.
-        # TODO: add some development mode where key errors are soft (because we
-        # won't know stuff like scriptid and slug until we actually make the kernel,
-        # which might not be until after we've done some local development)
+        # NB: Kind of an ugly ouroboros bootstrapping process here where we need some version of this
+        # metadata (with placeholder scriptids/slugs) in order to push initial kernel versions, which
+        # then lets us update this with the actual scriptids/slugs.
         dict(topic='syntax, variable assignment, and numbers',
             exercise=dict(
-                title='Exercises (Syntax, Variables, and Numbers)',
-                scriptid=TODO,
-                slug='colinmorris/foo-bar-exercises-1',
+                title='Exercise (Syntax, Variables, and Numbers)',
+                scriptid=1275163,
                 ),
             tutorial=dict(
                 title='Hello, Python',
-                slug='colinmorris/foo-bar-tut-1',
                 ),
+            # Keys later added to each of exercise and tutorial:
+            #   - filename (e.g. ex_1.ipynb)
+            #   - slug (e.g. colinmorris/hello-python)
+            #   - title (exercise only. set to "Exercise ({tutorial_title})")
             ),
 
         dict(topic='functions and getting help',
             exercise=dict(
-                scriptid=TODO,
-                slug='colinmorris/foo-bar-exercises-2',
+                scriptid=1275158,
                 ),
             tutorial=dict(
                 title='Functions and Getting Help',
-                slug='colinmorris/foo-bar-tut-2',
                 ),
             ),
         
         dict(topic='booleans and conditionals',
             exercise=dict(
-                scriptid=TODO,
-                slug='colinmorris/foo-bar-exercises-3',
+                scriptid=1275165,
                 ),
             tutorial=dict(
                 title='Booleans and Conditionals',
-                slug='colinmorris/foo-bar-tut-3',
                 ),
             ),
         
         dict(topic='lists and tuples',
             exercise=dict(
-                scriptid=TODO,
-                slug='colinmorris/foo-bar-exercises-4',
+                scriptid=1275173,
                 ),
             tutorial=dict(
                 title='Lists',
-                slug='colinmorris/foo-bar-tut-4',
                 ),
             ),
         
         dict(topic='loops and list comprehensions',
             exercise=dict(
-                scriptid=TODO,
-                slug='colinmorris/foo-bar-exercises-5',
+                scriptid=1275177,
                 ),
             tutorial=dict(
                 title='Loops and List Comprehensions',
-                slug='colinmorris/foo-bar-tut-5',
                 ),
             ),
         
         dict(topic='strings and dictionaries',
             exercise=dict(
-                scriptid=TODO,
-                slug='colinmorris/foo-bar-exercises-6',
+                scriptid=1275185,
                 ),
             tutorial=dict(
                 title='Strings and Dictionaries',
-                slug='colinmorris/foo-bar-tut-6',
                 ),
             ),
         
         dict(topic='imports',
             exercise=dict(
-                scriptid=TODO,
-                slug='colinmorris/foo-bar-exercises-7',
+                scriptid=1275190,
                 ),
             tutorial=dict(
                 title='Working with External Libraries',
-                slug='colinmorris/foo-bar-tut-7',
                 ),
             ),
 ]
 
 def slugify(title):
-    s = title.replace('(', '').replace(')', '').lower()
+    s = title.replace('(', '').replace(')', '').replace(',', '').lower()
     tokens = s.split()
     return '-'.join(tokens)
 
-for i, lesson in enumerate(lessons_metadata):
+for i, lesson in enumerate(lessons_meta):
     num = i + 1
     lesson['exercise']['filename'] = 'ex_{}.ipynb'.format(num)
     lesson['tutorial']['filename'] = 'tut_{}.ipynb'.format(num)
