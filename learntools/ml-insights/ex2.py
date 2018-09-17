2b6847bbb9663661b066a9197390f9d82a1c8686
Dan Becker <dansbecker@gmail.com>
2018-09-17 16:15:18 -0600

    Fix broken link

--- learntools/ml-insights/ex2.py
+++ learntools/ml-insights/ex2.py
@@ -12,11 +12,8 @@ class WhichFeaturesAreUseful(ThoughtExperiment):
     probably matter.
     """
 
-class FirstPermImportance(EqualityCheckProblem):
+class FirstPermImportance(CodingProblem):
     _vars = ['perm']
-    max_year_built = 2010
-    min_home_age = datetime.datetime.now().year - max_year_built
-    _expected = np.array([0.62288714, 0.8266946, 0.84735854, -0.00291397])
     _hint = 'The only thing you need to change is the first argument to `PermutationImportance()`. Find the right model name in the code above'
     _solution = CS(
 """
@@ -26,12 +23,52 @@ from eli5.sklearn import PermutationImportance
 perm = PermutationImportance(first_model, random_state=1).fit(val_X, val_y)
 eli5.show_weights(perm, feature_names = base_features)
 """)
+    def check(self, perm_obj):
+        assert np.allcose(perm_obj.feature_importances_, 
+                            [ 0.62288714,  0.8266946 ,  0.53837499,  0.84735854, -0.00291397], rtol=0.05)
 
-TODO: Add questions 3 and 4 HERE
+class WhyLatitude(ThoughtExperiment):
+    _solution = """
+    1. Travel might tend to have greater latitude distances than longitude distances. If the longitudes values were generally closer together, shuffling them wouldn't matter as much.
+    2. Different parts of the city might have different pricing rules (e.g. price per mile). If the divisions correspond more closely to latitude than longitude, that would cause latitude to matter more for accurate price prediction.
+    3. Tolls might be greater on roads going North<->South (changing latitude) than
+    on roads going East <-> West (changing longitude).  Thus latitude would have a larger effect on the prediction because it captures the amount of the tolls.
+    """
+
+class ImportanceWithAbsFeatures(CodingProblem):
+    _vars = ['perm2']
+    _hint = 'The only thing you need to change is the first argument to `PermutationImportance()`. Find the right model name in the code above'
+    _solution = CS(
+"""
+data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
+data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
+
+features_2  = ['pickup_longitude',
+               'pickup_latitude',
+               'dropoff_longitude',
+               'dropoff_latitude',
+               'abs_lat_change',
+               'abs_lon_change']
+
+X = data[features_2]
+new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_state=1)
+second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
+
+# Create a PermutationImportance object on second_model and fit it to new_val_X and new_val_y
+perm2 = PermutationImportance(second_model).fit(new_val_X, new_val_y)
+
+# show the weights for the permutation importance you just calculated
+eli5.show_weights(perm2, feature_names = features_2)
+""")
+    def check(self, perm_obj):
+        assert np.allcose(perm.feature_importances_,
+                            [ 0.62288714,  0.8266946 ,  0.53837499,  0.84735854, -0.00291397], rtol=0.05)
+
 
 qvars = bind_exercises(globals(), [
     WhichFeaturesAreUseful,
     FirstPermImportance,
+    WhyLatitude
     ],
     # tutorial_id=118,
     var_format='q_{n}',
