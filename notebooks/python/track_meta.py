0fb91e0ea8b92fd3d85bcad2d0e3476a55f1de4d
Colin Morris <colin.morris2@gmail.com>
2018-09-24 19:35:33 -0400

    python track stuff

---
+++ notebooks/python/track_meta.py
@@ -0,0 +1,29 @@
+track = dict(
+    author_username='colinmorris',
+)
+
+lessons = [
+        dict(topic='syntax, variable assignment, and numbers',),
+        dict(topic='functions and getting help',),
+        dict(topic='booleans and conditionals',),
+        dict(topic='lists and tuples',),
+        dict(topic='loops and list comprehensions',),
+        dict(topic='strings and dictionaries',),
+        dict(topic='imports',),
+]
+
+_tuts = [dict(lesson_idx=i, type='tutorial', filename='tut_{}.ipynb'.format(i+1))
+        for i in range(len(lessons))
+        ]
+_tuts[0]['title'] = 'Hello, Python'
+_tuts[3]['title'] = 'Lists'
+_tuts[6]['title'] = 'Working with External Libraries'
+
+_exercise_scriptids = [1275163, 1275158, 1275165, 1275173,
+        1275177, 1275185, 1275190]
+
+_exs = [dict(lesson_idx=i, scriptid=sid, type='exercise', filename='ex_{}.ipynb'.format(i+1)) 
+        for i, sid in enumerate(_exercise_scriptids)
+        ]
+notebooks = _tuts + _exs
+
