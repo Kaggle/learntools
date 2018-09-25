14b89210d923bf0a7319514346b47c8d50fef8f1
Colin Morris <colin.morris2@gmail.com>
2018-09-25 16:09:35 -0400

    bugfix

--- notebooks/clean.py
+++ notebooks/clean.py
@@ -30,4 +30,5 @@ def main():
         clean_nb(nb)
         nbf.write(nb, nbpath)
 
-main()
+if __name__ == '__main__':
+    main()
--- notebooks/render.py
+++ notebooks/render.py
@@ -13,7 +13,6 @@ CLEAN = 1
 # TODO: would be nice to do some Make-like shortcuts to avoid processing notebooks
 # whose rendered mtime > their partial mtime (and > the track meta mtime)
 def main():
-    # TODO: Call clean before preprocessing stuff.
     track = sys.argv[1]
     meta = utils.get_track_meta(track)
     track_cfg = utils.get_track_config(track)
