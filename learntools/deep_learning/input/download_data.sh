a5f7ba2396300282837060c3f98357aed836bbaa
Dan Becker <dansbecker@gmail.com>
2018-10-01 17:30:45 -0600

    Set-up DL notebooks to run locally

---
+++ learntools/deep_learning/input/download_data.sh
@@ -0,0 +1,19 @@
+kaggle competitions download dog-breed-identification -p ./dog_breed_identification -f train.zip
+unzip ./dog_breed_identification/train.zip -d ./dog_breed_identification/
+rm ./dog-breed-identification/train.zip
+
+kaggle datasets download keras/resnet50 -p resnet50
+unzip resnet50/resnet50.zip -d resnet50
+
+
+kaggle datasets download dansbecker/hot-dog-not-hot-dog -p hot-dog-not-hot-dog --unzip
+unzip hot-dog-not-hot-dog/hot-dog-not-hot-dog.zip -d hot-dog-not-hot-dog/
+
+
+kaggle datasets download dansbecker/dogs-gone-sideways -p dogs-gone-sideways --unzip
+unzip dogs-gone-sideways/images.zip -d dogs-gone-sideways/
+
+kaggle datasets download zalando-research/fashionmnist -p fashionmnist
+
+kaggle competitions download digit-recognizer -p ./digit-recognizer -f train.csv
+
