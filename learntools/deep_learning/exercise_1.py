c693b89e5298a21c2e8798928f0effdbf580ebcb
Dan Becker <dansbecker@gmail.com>
2018-10-04 06:50:45 -0600

    Clean up DL ex1. Start DL ex3.

--- learntools/deep_learning/exercise_1.py
+++ learntools/deep_learning/exercise_1.py
@@ -2,13 +2,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+from learntools.core.utils import bind_exercises
+from learntools.core.richtext import CodeSolution as CS
+from learntools.core.problem import *
+
+class VerticalLineDetector(CodingProblem):
+    _vars = ['original_image', 'vertical_line_conv']
+    _hint = ("Use `print(home_data.columns)`. The column you want is at the end "
+            "of the list. Use the dot notation to pull out this column from the DataFrame")
+    _solution = CS('')
+
+    def check(self, original_image, conv):
+        assert (isinstance(conv, list) or isinstance(conv, np.ndarray)), \
+               ("The input format should be a list of lists")
+        assert (conv[0][0] == conv[1][0]), \
+               ("Both numbers on the left of the convolution should be the same, to avoid picking up other patterns.")
+        assert (conv[0][1] == conv[1][1]), \
+               ("Both numbers on the right of the convolution should be the same, to avoid picking up other patterns.")
+        assert (np.sign(conv[0][0]) == -1 * np.sign(conv[0][1])), \
+               ("The numbers on the left and right should have different signs")
+
+    _hint = "You can use the same numbers that show up in horizontal_line_conv above. Just change where some of them are located."
+    _solution = CS(
+"""
+vertical_line_conv = [[1, -1],
+                      [1, -1]]
+
+"""
+)
+
+class BigVsSmallConvolutions(ThoughtExperiment):
+    _solution = \
+"""
+While any one convolution measures only a single pattern, there are more possible convolutions that can be created with large sizes.
+So there are also more patterns that can be captured with large convolutions.
+
+For example, it's possible to create a 3x3 convolution that filters for bright pixels with a dark one in the middle. There is no
+configuration of a 2x2 convolution that would capture this.
+
+On the other hand, anything that can be captured by a 2x2 convolution could also be captured by a 3x3 convolution.
+
+Does this mean powerful models require extremely large convolutions?  Not necessarily.
+In the next lesson, you will see how deep learning models put together many convolutions to capture complex patterns... including patterns to complex to be captured by any single convolution.
+"""
+
 
 def visualize_conv(image, conv):
     conv_array = np.array(conv)
     vertical_padding = conv_array.shape[0] - 1
     horizontal_padding = conv_array.shape[1] - 1
-    conv_out = scale_for_display(apply_conv_to_image(conv, image))
+    conv_out = scale_for_display(apply_conv_to_image(conv_array, image),
+                                contrast_factor=350)
     show(np.hstack([image[:-vertical_padding, :-horizontal_padding], conv_out]), False)
+
+qvars = bind_exercises(globals(), [
+    VerticalLineDetector,
+    BigVsSmallConvolutions,
+    ],
+    tutorial_id=74,
+    var_format='q_{n}',
+    )
+
 
 ########## CODE BELOW THIS LINE WAS USED IN PRE-LEARNTOOLS KERNELS ############
 ############################# MODIFY WITH CARE ################################
@@ -48,10 +102,10 @@ def print_hints(conv):
         # we've already filtered cases where first column and second column have same sign
         print("Congrats.  That did it.")
 
-def scale_for_display(image):
+def scale_for_display(image, contrast_factor=256):
     '''Scales numpy array containing image data to be integers in range [0, 256]'''
     out = image - image.min()
-    out = (out / out.max() * 256)
+    out = (out / out.max() * contrast_factor).clip(0, 256)
     return out.astype(int)
 
 
@@ -78,3 +132,7 @@ def show(image, scale_before_display=True):
     plt.imshow(to_display, cmap='gray')
     plt.axis('off')
     plt.show()
+
+# Also export functions needed to support pre-learntools kernels. Some may be used by new kernels too
+legacy_functions = ["visualize_conv", "load_my_image", "print_hints", "apply_conv_to_image", "show"]
+__all__ = list(qvars) + legacy_functions
