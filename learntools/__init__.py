4a4661fb6aff09e33c28f33eee89a4002794d5ab
Vincent Roseberry <rosbo@google.com>
2018-11-09 17:53:39 -0800

    Fix broken imports ml_explainability
    The content of learntools/ml_insights was migrated in #28 to
    learntools/ml_explainability. However, some imports were not updated.

--- learntools/__init__.py
+++ learntools/__init__.py
@@ -1,3 +1,3 @@
-from . import advanced_pandas, core, deep_learning, gans, machine_learning, python, embeddings, ml_insights
+from . import advanced_pandas, core, deep_learning, gans, machine_learning, python, embeddings, ml_explainability
 
 __version__ = '0.2.5'
