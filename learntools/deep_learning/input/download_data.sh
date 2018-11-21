94120c5ba307144264461bdbaee3971e8064d47e
Dan Becker <dansbecker@gmail.com>
2018-11-21 13:30:10 -0700

    add DL exercise 4 checking code

--- learntools/deep_learning/input/download_data.sh
+++ learntools/deep_learning/input/download_data.sh
@@ -3,10 +3,13 @@ unzip ./dog-breed-identification/train.zip -d ./dog-breed-identification/
 rm ./dog-breed-identification/train.zip
 
 kaggle datasets download keras/resnet50 -p resnet50
+unzip resnet50/resnet50.zip -d resnet50/
 
 kaggle datasets download -d keras/vgg16 -p vgg16
+unzip vgg16/vgg16.zip -d vgg16/
 
 kaggle datasets download -d keras/inceptionv3 -p inceptionv3
+unzip inceptionv3/inceptionv3.zip -d inceptionv3/
 
 kaggle datasets download dansbecker/hot-dog-not-hot-dog -p hot-dog-not-hot-dog/seefood --unzip
 
