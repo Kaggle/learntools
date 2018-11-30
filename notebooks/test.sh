0113d422af6231c796a40dcbb7d4f2d46c961186
Colin Morris <colin.morris2@gmail.com>
2018-11-30 14:25:26 -0500

    Uncomment some of notebook testing script

--- notebooks/test.sh
+++ notebooks/test.sh
@@ -6,23 +6,24 @@ set -x
 
 # path to the notebook/ directory.
 DIR=`dirname "${BASH_SOURCE[0]}"`
+cd $DIR
 TMP_DIR=`mktemp -d`
 
 # Install packages the notebook pipeline depends on but which aren't installed with the learntools package.
-pip3 install -r "$DIR/requirements.txt"
+pip3 install -r requirements.txt
 
-# TRACKS="embeddings pandas python"
-# for track in $TRACKS
-# do
-#     # Run each step of the rendering pipeline, to make sure it runs without errors.
-#     python3 clean.py $track
-#     python3 prepare_push.py $track
-#     python3 render.py $track
+TRACKS="embeddings pandas python"
+for track in $TRACKS
+do
+    # Run each step of the rendering pipeline, to make sure it runs without errors.
+    python3 clean.py $track
+    python3 prepare_push.py $track
+    python3 render.py $track
 
-#     # TODO: Once basic checks are working we should also do the following here:
-#     # - download necessary datasets for this track
-#     # - nbconvert execute *all* exercise notebooks for the track
-# done
+    # TODO: Once basic checks are working we should also do the following here:
+    # - download necessary datasets for this track
+    # - nbconvert execute *all* exercise notebooks for the track
+done
 
 # For now, just run one notebook (which doesn't depend on any datasets)
-jupyter nbconvert --output "$TMP_DIR" --execute "$DIR/python/raw/ex_2.ipynb"
+jupyter nbconvert --output "$TMP_DIR" --execute "python/raw/ex_2.ipynb"
