decb857cccdc0ce56aa2a128e1cfd23c2c3953ac
Dan Becker <dansbecker@gmail.com>
2018-12-06 15:42:42 -0700

    Use linking macros and add setup_data in DL course

--- notebooks/deep_learning/setup_data.sh
+++ notebooks/deep_learning/setup_data.sh
@@ -8,9 +8,6 @@ unzip resnet50/resnet50.zip -d resnet50/
 kaggle datasets download -d keras/vgg16 -p vgg16
 unzip vgg16/vgg16.zip -d vgg16/
 
-kaggle datasets download -d keras/inceptionv3 -p inceptionv3
-unzip inceptionv3/inceptionv3.zip -d inceptionv3/
-
 kaggle datasets download dansbecker/hot-dog-not-hot-dog -p hot-dog-not-hot-dog/seefood --unzip
 
 kaggle datasets download dansbecker/dogs-gone-sideways -p dogs-gone-sideways --unzip
