a2792a855b4fc651dec74b12d1adb9f710fa95f1
Alexis Cook <alexis.cook@gmail.com>
2019-01-25 19:00:46 -0600

    ex3 edits

--- learntools/data_viz_easy/ex3.py
+++ learntools/data_viz_easy/ex3.py
@@ -5,6 +5,8 @@ import seaborn as sns
 from learntools.core import *
 
 df = pd.read_csv("../input/cancer.csv", index_col="Id")
+df_b = pd.read_csv("../input/cancer_b.csv", index_col="Id")
+df_m = pd.read_csv("../input/cancer_m.csv", index_col="Id")
 
 class LoadCancerData(EqualityCheckProblem):
     _var = 'cancer_data'
@@ -58,24 +60,27 @@ sns.scatterplot(x=cancer_data['Perimeter (mean)'], y=cancer_data['Texture (worst
         assert len(legend_handles) == 3, "Did you color-code the points with the `'Diagnosis'` column?"
                 
 class ThinkScatter(ThoughtExperiment):
-    _hint = ("`'Perimeter (mean)'` is useful if the malignant and benign "
+    _hint = ("`'Perimeter (mean)'` is relatively useful if the malignant and benign "
              "tumors are separated relatively well in the scatter plot, where one type of tumor appears "
-             "mostly on the **right** of the chart, and the other appears mostly on the **left**; this means "
-             "that there's some value for `'Perimeter (mean)'` that separates the two types of tumors. "
+             "mostly on the **right** of the chart, and the other appears mostly on the **left**. "
              "Likewise, `'Texture (worst)'` is relatively useful if one type of tumor appears mostly "
-             "towards the **top** of the chart, and the other appears mostly towards the **bottom**; this means "
-             "that there's some value for `'Texture (worst)'` that separates the two types of tumors.")
-    _solution = ("`'Perimeter (mean)'` is the relatively more useful column, since values > 70 typically " 
-                 "indicate that a tumor is malignant.  Of course, this threshold value isn't perfect (since many "
-                 "benign tumors also have values > 70), but malignant tumors do tend to have relatively higher values for "
-                 "`'Perimeter (mean)'`.  There is no threshold value for `'Texture (worst)'` that does as good a job of "
-                 "separating the two types of tumors.")
+             "towards the **top** of the chart, and the other appears mostly towards the **bottom**.")
+    _solution = ("`'Perimeter (mean)'` is the relatively more useful column, since malignant tumors typically have "
+                 "larger values in this column (and therefore appear mostly towards the right of the scatter plot). "
+                 "While there's no perfect threshold that perfectly separates the two types of tumors, "
+                 "the value of this column does provide _some_ indication of how the tumor should be diagnosed. "
+                 "In contrast, `'Texture (worst)'` does not do as good of a job of separating the two types of tumors. "
+                 "(_**Side Note**: In practice,  the most powerful algorithms are able to find patterns in the data "
+                 "that are not immediately visible to humans -- so while it may not initially be obvious to "
+                 "a human eye how `'Texture (worst)'` can be used to classify tumors, the machine learning algorithm may "
+                 "be able to figure it out by studying all of the data to find more complex patterns "
+                 "that involve multiple columns at once!_)")
     
 Scatter = MultipartProblem(PlotScatter, ThinkScatter)
 
 class SwarmPlot(CodingProblem):
     _var = 'plt'
-    _hint = ("Use `sns.swarmplot`, and set the variables for the x-axis, and y-axis "
+    _hint = ("Use `sns.swarmplot`, and set the variables for the x-axis and y-axis "
         "by using `x=` and `y=`, respectively.")
     _solution = CS(
 """# Scatter plot showing the relationship between 'Diagnosis' and 'Perimeter (mean)'
@@ -92,11 +97,42 @@ sns.swarmplot(x=cancer_data['Perimeter (mean)'], y=cancer_data['Diagnosis'])
         print("Thank you for creating a chart!  To see how your code compares to the official "
               "solution, please use the code cell below.")
 
+class PlotThreshold(CodingProblem):
+    _var = 'plt'
+    _hint = ("Use `sns.kdeplot`, and specify the data and label by using `data=` and `label=`, respectively. You will need to "
+             "write two lines of code, corresponding to `cancer_m_data` and `cancer_b_data`.")
+    _solution = CS(
+"""# KDE plot for benign and malignant tumors
+sns.kdeplot(data=cancer_b_data['Radius (worst)'], shade=True, label="Benign")
+sns.kdeplot(data=cancer_m_data['Radius (worst)'], shade=True, label="Malignant")
+""")
+
+    def solution_plot(self):
+        self._view.solution()
+        sns.kdeplot(data=df_b['Radius (worst)'], shade=True, label="Benign")
+        sns.kdeplot(data=df_m['Radius (worst)'], shade=True, label="Malignant")
+    
+    def check(self, passed_plt):
+        assert len(passed_plt.figure(1).axes) > 0, "Please write code to create a KDE plot."
+        
+        print("Thank you for creating a plot!  To see how your code compares to the official "
+              "solution, please use the code cell below.")
+        
+class ThinkThreshold(ThoughtExperiment):
+    _hint = ("Take a look at the KDE plot, and use the legend to tell the difference between malignant and benign tumors.  Does "
+             "the curve for malignant tumors appear to the right (at higher values) or to the left (at lower values) of "
+             "the curve for benign tumors?")
+    _solution = ("Malignant tumors tend to have higher values for `'Radius (worst)'`.  This is something that we'd like a "
+                 "machine learning algorithm to learn for itself when studying the data!")
+
+Threshold = MultipartProblem(PlotThreshold, ThinkThreshold)
+    
 qvars = bind_exercises(globals(), [
     LoadCancerData,
     ReviewData,
     Scatter,
-    SwarmPlot
+    SwarmPlot,
+    Threshold
     ],
     tutorial_id=-1,
     var_format='step_{n}',
