9d1ad99c294d27d4a9198783b09ac4e1be1ac7ae
Dan Becker <dansbecker@gmail.com>
2019-02-05 14:29:46 -0700

    Fix course name in track_meta for ML_explainability

--- notebooks/ml_explainability/track_meta.py
+++ notebooks/ml_explainability/track_meta.py
@@ -1,8 +1,8 @@
 # See also examples/example_track/example_meta.py for a longer, commented example
 track = dict(
     author_username='dansbecker',
-    course_name='Machine Learning',
-    course_url='https://www.kaggle.com/learn/machine-learning'
+    course_name='Machine Learning Explainability',
+    course_url='https://www.kaggle.com/learn/machine-learning-explainability'
 )
 
 lessons = [ {'topic': topic_name} for topic_name in
