f0f550ae7996266ccd666236a49dc48cb8005533
Dan Becker <dansbecker@gmail.com>
2019-03-25 18:03:32 -0600

    Start Leakage exercise

---
+++ learntools/ml_level_2/ex7.py
@@ -0,0 +1,19 @@
+from learntools.core import *
+
+class WeatherIsLeakage(ThoughtExperiment):
+    _hint = (
+    """
+
+    """)
+    _solution = (
+    """
+    answer here
+    """)
+
+qvars = bind_exercises(globals(), [
+    WeatherIsLeakage
+    ],
+    tutorial_id=-1,
+    var_format='step_{n}',
+    )
+__all__ = list(qvars)
