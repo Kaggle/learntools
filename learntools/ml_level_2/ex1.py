351b9dc182fdbf1978761c4b8fe36d03895f82aa
Alexis Cook <alexis.cook@gmail.com>
2019-03-26 17:04:37 -0600

    tut1, ex1

--- learntools/ml_level_2/ex1.py
+++ learntools/ml_level_2/ex1.py
@@ -25,34 +25,16 @@ class BestModel(CodingProblem):
 
 class Predictions(CodingProblem):
     _var = 'my_model'
-    _hint = ("You need only set `my_model` to a random forest model.  The rest of the code "
-             "is written for you!")
+    _hint = ("You need only set `my_model` to a random forest model.  You are welcome (but "
+             "not required) to choose one of the five models above.")
     _solution = CS(
 """# Define a model
 my_model = best_model
-
-try: 
-    # Fit the model on the training and validation data
-    my_model.fit(X, y)
-    # Generate test predictions
-    preds_test = my_model.predict(X_test)
-    # Save predictions in format used for competition scoring
-    output = pd.DataFrame({'Id': X_test.index,
-                       'SalePrice': preds_test})
-    output.to_csv('submission.csv', index=False)
-except AttributeError:
-    pass
 """)
     
     def check(self, my_model):
-        assert 'sklearn' in str(type(my_model)), \
-        "The model defined in `my_model` does not appear to be a valid scikit-learn model!"
-        
-        df = pd.read_csv('./submission.csv')
-        assert os.path.isfile('./submission.csv') and list(df.columns) == ['Id', 'SalePrice'] \
-        and len(df) == 1459, \
-        ("It looks like you edited the provided code that creates the submission file.  Please "
-         "convert it back to the original.")
+        assert type(my_model) == RandomForestRegressor, \
+        "Please change `my_model` to a random forest model."
 
 qvars = bind_exercises(globals(), [
     BestModel,
