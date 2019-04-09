5dcdda65038ab64667ca51f56abf6722bee41c64
Alexis Cook <alexis.cook@gmail.com>
2019-04-09 13:11:04 -0500

    ex6

--- learntools/sql/ex6.py
+++ learntools/sql/ex6.py
@@ -20,19 +20,25 @@ class ListSOTables(EqualityCheckProblem):
  'tags',
  'users',
  'votes']
-    _solution = CS('list_of_tables = stack_overflow.list_tables()')
+    _solution = CS(
+"""
+# Get a list of available tables 
+tables = list(client.list_tables(dataset))
+list_of_tables = [table.table_id for table in tables] 
+"""
+)
 
 class HowToFindExperts(ThoughtExperiment):
     _solution = \
 """
 `posts_questions` has a column called `tags` which lists the topics/technologies each question is about.
 
-`posts_answers` has a column called `parent_id` which identifies the id of the question each answer is responding to.
-`posts_answers` also has an `owner_user_id` column which specifies the user_id that wrote each answer.
+`posts_answers` has a column called `parent_id` which identifies the ID of the question each answer is responding to.
+`posts_answers` also has an `owner_user_id` column which specifies the ID of the user who answered the question.
 
-You can join these two tables to determine the `tags` for each answer.
-
-Then select the `owner_user_id` of the answers on the desired tag.
+You can join these two tables to:
+- determine the `tags` for each answer, and then
+- select the `owner_user_id` of the answers on the desired tag.
 
 This is exactly what you will do over the next few questions.
 """
@@ -42,22 +48,25 @@ class SelectRightQuestions(CodingProblem):
     def check(self, query, results):
         lower_query = query.lower()
         results.columns = [c.lower() for c in results.columns]
-        assert ('like \'%bigquery%\'' in lower_query), ('Your `WHERE` clause is not filtering on the bigquery tag correctly')
+        assert ('like \'%bigquery%\'' in lower_query), ('Your **WHERE** clause is not filtering on the bigquery tag correctly')
         assert ('id' in results.columns), ('Should have `id` in the response columns. Your column names are {}'.format(results.columns))
         assert (34798244 in results.id.values), ('You seem to be missing some relevant values from the id columns')
         assert (results.shape[1] == 3), ('You should have 3 columns. But you have {}. Your list of columns is'.format(len(results.columns), results.columns))
-        assert (results.shape[0] < 20000), ('Your results have too many rows in the response. You may not have the right WHERE clause')
-    _hint = 'Your WHERE clause should be `WHERE tags like \'%bigquery%\'`'
+        assert (results.shape[0] < 20000), ('Your results have too many rows in the response. You may not have the right **WHERE** clause')
+    _hint = 'Your **WHERE** clause should be `WHERE tags LIKE \'%bigquery%\'`'
     _solution = CS(
 """
-questions_query = \
-\"""
-SELECT id, title, owner_user_id
-FROM `bigquery-public-data.stackoverflow.posts_questions`
-WHERE tags like '%bigquery%'
-\"""
+questions_query = \"""
+                  SELECT id, title, owner_user_id
+                  FROM `bigquery-public-data.stackoverflow.posts_questions`
+                  WHERE tags like '%bigquery%'
+                  \"""
 
-questions_results=stack_overflow.query_to_pandas_safe(my_query, max_gb_scanned=2)
+# Set up the query
+questions_query_job = client.query(questions_query)
+
+# API request - run the query, and return a pandas DataFrame
+questions_results = questions_query_job.to_dataframe()
 """
 )
 
@@ -73,36 +82,51 @@ class FirstJoin(CodingProblem):
         assert (results.shape[0] < 20000), ('You have {} rows in your results. It should be closer to 10500. You may the wrong WHERE clause')
 
 
-    _hint = """Do an inner JOIN between `bigquery-public-data.stackoverflow.posts_questions` and  `bigquery-public-data.stackoverflow.posts_answers`.
+    _hint = \
+"""
+Do an **INNER JOIN** between `bigquery-public-data.stackoverflow.posts_questions` and `bigquery-public-data.stackoverflow.posts_answers`.
 
-    You will want to give both of them aliases. Call `post_questions` q and call `posts_answers` a. The `ON` part of your join is `q.id = a.parent_id`
-    """
+Give `post_questions` an alias of `q`, and use `a` as an alias for `posts_answers`. The **ON** part of your join is `q.id = a.parent_id`.
+"""
     _solution = CS(\
 """
 answers_query = \"""
-SELECT a.id, a.body, a.owner_user_id
-FROM `bigquery-public-data.stackoverflow.posts_questions` q INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` a
-     ON q.id = a.parent_id
-     WHERE q.tags like '%bigquery%'
-\"""
+                SELECT a.id, a.body, a.owner_user_id
+                FROM `bigquery-public-data.stackoverflow.posts_questions` AS q 
+                INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a
+                    ON q.id = a.parent_id
+                WHERE q.tags LIKE '%bigquery%'
+                \"""
+                
+# Set up the query
+answers_query_job = client.query(answers_query)
+
+# API request - run the query, and return a pandas DataFrame
+answers_results = answers_query_job.to_dataframe()
 """
 )
 
 class BigQueryExperts(CodingProblem):
     _vars = ['bigquery_experts_query', 'bigquery_experts_results']
-    _hint = "Start with `SELECT a.owner_user_id AS user_id, COUNT(1) number_of_answers`"
+    _hint = "Start with `SELECT a.owner_user_id AS user_id, COUNT(1) AS number_of_answers`"
     _solution = CS(
 """
-bigquery_experts_query = \"""SELECT a.owner_user_id AS user_id, COUNT(1) number_of_answers
-                  FROM `bigquery-public-data.stackoverflow.posts_questions` q
-                        INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` a
-                        ON q.id = a.parent_Id
-                  WHERE q.tags like '%bigquery%'
-                  GROUP BY a.owner_user_id\"""
+bigquery_experts_query = \"""
+                         SELECT a.owner_user_id AS user_id, COUNT(1) number_of_answers
+                         FROM `bigquery-public-data.stackoverflow.posts_questions` q
+                         INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` a
+                             ON q.id = a.parent_Id
+                         WHERE q.tags like '%bigquery%'
+                         GROUP BY a.owner_user_id
+                         \"""
 
-bigquery_experts_results = stack_overflow.query_to_pandas_safe(bigquery_experts_query, max_gb_scanned=2)
+# Set up the query
+bigquery_experts_query_job = client.query(bigquery_experts_query)
+
+# API request - run the query, and return a pandas DataFrame
+bigquery_experts_results = bigquery_experts_query_job.to_dataframe()
 """
-    )
+)
     def check(self, query, results):
         lower_query = query.lower()
         results.columns = [c.lower() for c in results.columns]
@@ -122,27 +146,33 @@ bigquery_experts_results = stack_overflow.query_to_pandas_safe(bigquery_experts_
 class GeneralizeExpertFinder(ThoughtExperiment):
     _solution = CS(
 """
-def expert_finder(topic, stack_overflow_helper):
+def expert_finder(topic, client):
     '''
-    Returns a DataFrame with the user_id's who have written stackoverflow answers on topic.
+    Returns a DataFrame with the user IDs who have written Stack Overflow answers on a topic.
 
     Inputs:
-        topic: A string with the topic we are interested
-        stack_overflow_helper: A bigquery_helper object that specifies the connection to the stack overflow DB
+        topic: A string with the topic of interest
+        client: A Client object that specifies the connection to the Stack Overflow dataset
 
     Outputs:
         results: A DataFrame with columns for user_id and number_of_answers. Follows similar logic to bigquery_experts_results shown above.
     '''
-    my_query = \"""SELECT a.owner_user_id AS user_id, COUNT(1) AS number_of_answers
-                  FROM `bigquery-public-data.stackoverflow.posts_questions` q
-                        INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` a
-                        ON q.id = a.parent_Id
-                  WHERE q.tags like '%' + tag + '%'
-                  GROUP BY a.owner_user_id
-                \"""
-    # a real service would have good error handling for queries that scan too much data
-    my_results = stack_overflow_helper.query_to_pandas_safe(my_query, max_gb_scanned=2)
-    return my_results
+    my_query = \"""
+               SELECT a.owner_user_id AS user_id, COUNT(1) AS number_of_answers
+               FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+               INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a
+                   ON q.id = a.parent_Id
+               WHERE q.tags like '%' + tag + '%'
+               GROUP BY a.owner_user_id
+               \"""
+               
+    # Set up the query      
+    my_query_job = client.query(my_query)
+    
+    # API request - run the query, and return a pandas DataFrame
+    results = my_query_job.to_dataframe()
+
+    return results
 """
 )
 
