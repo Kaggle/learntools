da86b714eb8309256b70b078a4c470ef1fb4fd4f
Alexis Cook <alexis.cook@gmail.com>
2019-04-10 14:14:00 -0500

    ex6 checking code

--- learntools/sql/ex6.py
+++ learntools/sql/ex6.py
@@ -1,25 +1,52 @@
+from google.cloud import bigquery
+
 from learntools.core import *
+
+# Setup (7.42s on Kaggle)
+client = bigquery.Client()
+
+# (1) ListSOTables
+dataset_ref = client.dataset("stackoverflow", project="bigquery-public-data")
+dataset = client.get_dataset(dataset_ref)
+tables = list(client.list_tables(dataset))
+list_of_tables_answer = [table.table_id for table in tables] 
+
+# (3) SelectRightQuestions
+questions_query = """
+                  SELECT id, title, owner_user_id
+                  FROM `bigquery-public-data.stackoverflow.posts_questions`
+                  WHERE tags like '%bigquery%'
+                  """
+questions_query_job = client.query(questions_query)
+questions_answer = questions_query_job.to_dataframe()
+
+# (4) FirstJoin
+answers_query = """
+                SELECT a.id, a.body, a.owner_user_id
+                FROM `bigquery-public-data.stackoverflow.posts_questions` AS q 
+                INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a
+                    ON q.id = a.parent_id
+                WHERE q.tags LIKE '%bigquery%'
+                """
+answers_query_job = client.query(answers_query)
+answers_answer = answers_query_job.to_dataframe()
+
+# (5) BigQueryExperts
+bigquery_experts_query = """
+                         SELECT a.owner_user_id AS user_id, COUNT(1) AS number_of_answers
+                         FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+                         INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a
+                             ON q.id = a.parent_Id
+                         WHERE q.tags LIKE '%bigquery%'
+                         GROUP BY a.owner_user_id
+                         """
+bigquery_experts_query_job = client.query(bigquery_experts_query)
+bigquery_experts_answer = bigquery_experts_query_job.to_dataframe()
 
 
 class ListSOTables(EqualityCheckProblem):
     _var = 'list_of_tables'
-    _expected = \
-['badges',
- 'comments',
- 'post_history',
- 'post_links',
- 'posts_answers',
- 'posts_moderator_nomination',
- 'posts_orphaned_tag_wiki',
- 'posts_privilege_wiki',
- 'posts_questions',
- 'posts_tag_wiki',
- 'posts_tag_wiki_excerpt',
- 'posts_wiki_placeholder',
- 'stackoverflow_posts',
- 'tags',
- 'users',
- 'votes']
+    _expected = list_of_tables_answer
     _solution = CS(
 """
 # Get a list of available tables 
@@ -48,18 +75,20 @@ class SelectRightQuestions(CodingProblem):
     def check(self, query, results):
         lower_query = query.lower()
         results.columns = [c.lower() for c in results.columns]
-        assert ('like \'%bigquery%\'' in lower_query), ('Your **WHERE** clause is not filtering on the bigquery tag correctly')
-        assert ('id' in results.columns), ('Should have `id` in the response columns. Your column names are {}'.format(results.columns))
-        assert (34798244 in results.id.values), ('You seem to be missing some relevant values from the id columns')
-        assert (results.shape[1] == 3), ('You should have 3 columns. But you have {}. Your list of columns is'.format(len(results.columns), results.columns))
-        assert (results.shape[0] < 20000), ('Your results have too many rows in the response. You may not have the right **WHERE** clause')
-    _hint = 'Your **WHERE** clause should be `WHERE tags LIKE \'%bigquery%\'`'
+        assert ('like \'%bigquery%\'' in lower_query), ('Your **WHERE** clause is not filtering on the "bigquery" tag correctly.')
+        assert ('id' in results.columns), ('Should have `id` in the columns. Your column names are {}.'.format(results.columns))
+        assert (results.shape[1] == 3), ('You should have 3 columns. But you have {}. Your list of columns is {}.'.format(len(results.columns), results.columns))
+        if not results.equals(questions_answer):
+            assert (34798244 in results.id.values), ('You seem to be missing some relevant values from the `id` column.')
+            assert (results.shape[0] < 20000), ('Your results have too many rows in the response. You may not have the right **WHERE** clause.')
+        assert (results.equals(questions_answer)), ("The results don't look right. Try again.")
+    _hint = 'Your **WHERE** clause should be `WHERE tags LIKE \'%bigquery%\'`.'
     _solution = CS(
 """
 questions_query = \"""
                   SELECT id, title, owner_user_id
                   FROM `bigquery-public-data.stackoverflow.posts_questions`
-                  WHERE tags like '%bigquery%'
+                  WHERE tags LIKE '%bigquery%'
                   \"""
 
 # Set up the query
@@ -75,13 +104,13 @@ class FirstJoin(CodingProblem):
     def check(self, query, results):
         lower_query = query.lower()
         results.columns = [c.lower() for c in results.columns]
-        assert ('like \'%bigquery%\'' in lower_query), ('Your `WHERE` clause is not filtering on the bigquery tag correctly')
-        assert ('join' in lower_query), ('Your query does not include a join statement')
-        assert ('id' in results.columns), ('You should have a column named `answer_id`. Your columns are {}'.format(results.columns))
-        assert (21592157 in results.id.values), ('You seem to be missing some relevant values from the `answer_id` columns')
-        assert (results.shape[0] < 20000), ('You have {} rows in your results. It should be closer to 10500. You may the wrong WHERE clause')
-
-
+        assert ('like \'%bigquery%\'' in lower_query), ('Your **WHERE** clause is not filtering on the "bigquery" tag correctly.')
+        assert ('join' in lower_query), ('Your query does not include a **JOIN** statement.')
+        assert ('id' in results.columns), ('You should have a column named `id`. Your columns are {}.'.format(results.columns))
+        if not results.equals(answers_answer):
+            assert (21592157 in results.id.values), ('You seem to be missing some relevant values from the `id` column.')
+            assert (results.shape[0] < 20000), ('You have {} rows in your results. It should be closer to 10500. You may have the wrong **WHERE** clause.')
+        assert (results.equals(answers_answer)), ("The results don't look right. Try again.")
     _hint = \
 """
 Do an **INNER JOIN** between `bigquery-public-data.stackoverflow.posts_questions` and `bigquery-public-data.stackoverflow.posts_answers`.
@@ -112,11 +141,11 @@ class BigQueryExperts(CodingProblem):
     _solution = CS(
 """
 bigquery_experts_query = \"""
-                         SELECT a.owner_user_id AS user_id, COUNT(1) number_of_answers
-                         FROM `bigquery-public-data.stackoverflow.posts_questions` q
-                         INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` a
+                         SELECT a.owner_user_id AS user_id, COUNT(1) AS number_of_answers
+                         FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+                         INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a
                              ON q.id = a.parent_Id
-                         WHERE q.tags like '%bigquery%'
+                         WHERE q.tags LIKE '%bigquery%'
                          GROUP BY a.owner_user_id
                          \"""
 
@@ -130,17 +159,18 @@ bigquery_experts_results = bigquery_experts_query_job.to_dataframe()
     def check(self, query, results):
         lower_query = query.lower()
         results.columns = [c.lower() for c in results.columns]
-        assert ('group by' in lower_query), ('Your query should have a GROUP BY clause')
-        assert ('count' in lower_query), ('Your query should have a COUNT in the SELECT statement')
-        assert ('%bigquery' in lower_query), ('Your query should have `tags like %bigquery%` in the WHERE clause')
-
-        assert ('user_id' in results.columns), ('You do not have a `user_id` column in your results')
-        assert ('number_of_answers' in results.columns), ('You do not have a `number_of_answers` column in your results')
-        # Correct answer has 2151 rows at time this code was written
-        rows_in_result = results.shape[0]
-        assert (rows_in_result < 10000), ('Your result has too many rows ({} rows). Something is wrong'.format(rows_in_result))
-        assert (rows_in_result > 2000), ('Your result has too few rows ({} rows). Something is wrong'.format(rows_in_result))
-        assert (212435 in results.user_id.values), ('Your results did not return the right set of user id\'s.')
+        assert ('group by' in lower_query), ('Your query should have a **GROUP BY** clause.')
+        assert ('count' in lower_query), ('Your query should have a **COUNT** in the **SELECT** statement.')
+        assert ('%bigquery' in lower_query), ('Your **WHERE** clause is not filtering on the "bigquery" tag correctly.')
+        assert ('user_id' in results.columns), ('You do not have a `user_id` column in your results.')
+        assert ('number_of_answers' in results.columns), ('You do not have a `number_of_answers` column in your results.')
+        if not results.equals(bigquery_experts_answer):
+            # Correct answer has 2151 rows at time this code was written
+            rows_in_result = results.shape[0]
+            assert (rows_in_result < 10000), ('Your result has too many rows ({} rows). Something is wrong'.format(rows_in_result))
+            assert (rows_in_result > 2000), ('Your result has too few rows ({} rows). Something is wrong'.format(rows_in_result))
+            assert (212435 in results.user_id.values), ('Your results did not return the right set of user id\'s.')
+        assert (results.equals(bigquery_experts_answer)), ("The results don't look right. Try again.")
 
 
 class GeneralizeExpertFinder(ThoughtExperiment):
