6f589eef366deb623c906ada011ab57638e89df7
Dan Becker <dansbecker@gmail.com>
2019-05-13 18:22:39 -0600

    First take on rebuilding pandas daily

--- notebooks/pandas/track_meta.py
+++ notebooks/pandas/track_meta.py
@@ -20,42 +20,28 @@ data-types-and-missing-data-workbook
 renaming-and-combining-reference
 renaming-and-combining-workbook""".split()
 
-# Mapping from exercise slugs to extra datasets they require (beyond the baseline wine-reviews
-# dataset used by all notebooks)
-exercise_aux_datasets = {
-        'creating-reading-and-writing-workbook': ['nolanbconaway/pitchfork-data'],
-        'renaming-and-combining-workbook': ['dansbecker/powerliftingdatabase',
-                                    'residentmario/things-on-reddit'],
-}
+datasets = ['zynicide/wine-reviews',
+            'nolanbconaway/pitchfork-data',
+            'dansbecker/powerlifting-database',
+            'residentmario/things-on-reddit',
+            'jpmiller/publicassistance',
+            'rtatman/188-million-us-wildfires',
+            'residentmario/ramen-ratings',
+            'datasnaek/chess',
+            'nasa/kepler-exoplanet-search-results',
+            'datasnaek/youtube-new',
+            ]
 
 import os; import json
 def _make_notebook(slug, type_, lesson_idx):
-    # XXX
-    # I guess this is most likely to be run from the notebooks directory, so
-    # make path relative to that.
     path = os.path.join('pandas', 'old_metadata', slug, 'kernel-metadata.json')
     with open(path) as f:
         ex = json.load(f)
-
-    datasets = ex.get('dataset_sources', [])
-    # The extant metadata included a lot of unused datasets for exercises. Trim
-    # them down.
-    if type_ == 'exercise':
-        # The "advanced pandas exercises" dataset is no longer used, but we'll keep
-        # using it in all exercise workbooks as a hack to make sure they all have
-        # at least 2 datasets, for consistency of input file paths.
-        adpan = 'residentmario/advanced-pandas-exercises'
-        datasets = [adpan, 'zynicide/wine-reviews']
-        extras = exercise_aux_datasets.get(slug, [])
-        datasets.extend(extras)
-        assert len(datasets) >= 2
-
     return dict(
             filename=slug+'.ipynb',
             type=type_,
             lesson_idx=lesson_idx,
             dataset_sources=datasets,
-            keywords=ex.get('keywords', []),
             title=ex['title'],
             scriptid=ex.get('id_no', -1),
             )
@@ -63,11 +49,11 @@ def _make_notebook(slug, type_, lesson_idx):
 type_order = ['tutorial', 'exercise']
 for i in range(0, len(SLUGS), 2):
     slugs = SLUGS[i:i+2]
-    slug = slugs[0]
+    tut_slug = slugs[0]
     suff = '-reference'
-    if slug.endswith(suff):
-        slug = slug[:-len(suff)]
-    topic = slug.replace('-', ' ')
+    if tut_slug.endswith(suff):
+        tut_slug = slug[:-len(suff)]
+    topic = tut_slug.replace('-', ' ')
     lesson = dict(topic=topic)
     lessons.append(lesson)
     lesson_idx = len(lessons) - 1
