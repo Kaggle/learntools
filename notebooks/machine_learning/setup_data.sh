19d79f37e9ecf25ed3d92d5cddb435397fd51db9
Dan Becker <dansbecker@gmail.com>
2019-09-19 14:26:38 -0600

    Update paths in ml setup_data.sh due to CLI change

--- notebooks/machine_learning/setup_data.sh
+++ notebooks/machine_learning/setup_data.sh
@@ -27,9 +27,10 @@ do
     dest="input/$comp"
     mkdir -p $dest
     kaggle competitions download $comp -p $dest
+    cd $dest
+    unzip ${comp}.zip
+    cp *.csv ..
+    cd ..
 done
-
-# the last exercise has only 1 dataset, so everything goes in input/ directly
-cp input/home-data-for-ml-course/* input
 
 cd ..
