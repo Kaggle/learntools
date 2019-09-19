0b31d0abf5f4df97637d5834678364de2fa1db6d
Alexis Cook <alexis.cook@gmail.com>
2019-09-19 14:39:25 -0500

    fix setup data

--- notebooks/geospatial/setup_data.sh
+++ notebooks/geospatial/setup_data.sh
@@ -1,5 +1,5 @@
 rm -r ./input
 mkdir input
 kaggle datasets download alexisbcook/geospatial-learn-course-data -p input
-unzip -q input/geospatial-learn-course-data.zip -d input
-chmod 644 input/*.csv
+unzip -q input/geospatial-learn-course-data.zip -d input/geospatial-learn-course-data
+chmod 644 input/geospatial-learn-course-data/*.csv
