da1cdba32e1da2c9d44e04e5832ad8e8f0332006
Alexis Cook <alexis.cook@gmail.com>
2019-09-20 14:32:00 -0500

    ex4_files added to dataset now

--- learntools/geospatial/tools.py
+++ learntools/geospatial/tools.py
@@ -1,17 +1,15 @@
 import geopandas as gpd
 
+all_addresses = ['2224 Shattuck Avenue Berkeley CA',
+                 '1799 Solano Avenue Berkeley CA',
+                 '1444 Shattuck Place Berkeley CA',
+                 '3001 Telegraph Avenue Berkeley CA',
+                 '2128 Oxford Street Berkeley CA']
+
 def geocode(address, provider="nominatim"):
-    all_addresses = ['2224 Shattuck Avenue Berkeley CA',
-       '1799 Solano Avenue Berkeley CA',
-       '1444 Shattuck Place Berkeley CA',
-       '3001 Telegraph Avenue Berkeley CA',
-       '2128 Oxford Street Berkeley CA']
-    
-    print(address)
-    print(provider)
-    print("address in addresses?", (address in all_addresses))
-    my_index = all_addresses.index(address)
-    print(my_index)
-    gdf = gpd.read_file("../../notebooks/geospatial/ex4_files/add_{}.shp".format(my_index))
-    print(gdf.head())
-    return gdf
+    try:
+        my_index = all_addresses.index(address)
+        gdf = gpd.read_file("../input/geospatial-learn-course-data/ex4_files/ex4_files/add_{}.shp".format(my_index))
+        return gdf
+    except:
+        return None
--- notebooks/geospatial/setup_data.sh
+++ notebooks/geospatial/setup_data.sh
@@ -1,5 +1,21 @@
-rm -r ./input
-mkdir input
-kaggle datasets download alexisbcook/geospatial-learn-course-data -p input
-unzip -q input/geospatial-learn-course-data.zip -d input/geospatial-learn-course-data
-chmod 644 input/geospatial-learn-course-data/*.csv
+#!/bin/bash
+# Download the datasets used in the ML notebooks to correct relative_paths (../input/...)
+
+set -e
+
+if [ -d input ]
+then
+    exit 0
+fi
+
+mkdir -p input
+
+DATASETS="alexisbcook/geospatial-learn-course-data"
+
+for slug in $DATASETS
+do
+    name=`echo $slug | cut -d '/' -f 2`
+    dest="input/$name"
+    mkdir -p $dest
+    kaggle d download -p $dest --unzip $slug
+done
