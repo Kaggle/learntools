40aa521d5da0dd81ec41e3ed90f03c36015d4a8b
Alexis Cook <alexis.cook@gmail.com>
2020-01-23 12:16:43 -0600

    switching in kaggle_envs

--- notebooks/rl/raw/kaggle_simulations/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import *
-import kaggle_simulations.utils as utils
-import kaggle_simulations.errors as errors
-
-__all__ = ["errors", "Environment", "evaluate", "make", "run", "utils"]
--- notebooks/rl/raw/kaggle_simulations/tests/test_errors.py
+++
@@ -1,9 +0,0 @@
-# from kaggle_simulations import errors
-
-
-# def test_unknown_error():
-#     err = errors.CanonicalError("foobar")
-#     assert err.name == "Unknown"
-#     assert err.message == "foobar"
-#     assert err.code == 2
-#     assert err.status == "UNKNOWN"
