c1de03cb5f9a4a55b08346661da911a7c2489c1f
Alexis Cook <alexis.cook@gmail.com>
2020-01-27 10:16:59 -0600

    edits

--- learntools/rl/ex3.py
+++ learntools/rl/ex3.py
@@ -1,17 +1,21 @@
 from learntools.core import *
 
+class WorseHeuristic(ThoughtExperiment):
+    _hint = ("The first heuristic assigns a score of 0 to column 2, and a score of -99 to "
+             "column 3.  What scores do you get with the second heuristic?")
+    _solution = ("The first heuristic is guaranteed to select column 2 to block "
+                 "the opponent from winning.  The second heuristic selects either "
+                 "column 2 or column 3 (where it selects each with 50% probability). "
+                 "Thus, for this game board, the first heuristic is better. In general, "
+                 "we can expect that the first heuristic is a better heuristic, "
+                 "since we cannot trust the second heuristic to block the opponent "
+                 "from winning.") 
+
 class NumLeaves(EqualityCheckProblem):
     _var = "num_leaves"
     _expected = 7**3
     _hint =  "Try drawing the game tree.  How many moves (columns) are possible at each turn?"
     _solution = CS("num_leaves = 7*7*7")
-    
-class AlphaBeta(CodingProblem):
-    _var = "tbd"
-    _hint = ""
-    _solution = ""
-    def check(self):
-        pass
 
 class JustSubmitEx3(CodingProblem):
     _hint = "Follow the instructions to submit your agent to the competition."
@@ -22,8 +26,8 @@ class JustSubmitEx3(CodingProblem):
         pass
 
 qvars = bind_exercises(globals(), [
+    WorseHeuristic,
     NumLeaves, 
-    AlphaBeta,
     JustSubmitEx3
     ],
     var_format='q_{n}',
