914ea315a54bc6ac8081b19a15948c91c068e786
Alexis Cook <alexis.cook@gmail.com>
2020-02-13 13:27:07 -0600

    adding links, more questions ex3

--- learntools/rl/ex3.py
+++ learntools/rl/ex3.py
@@ -16,11 +16,27 @@ class NumLeaves(EqualityCheckProblem):
     _expected = 7**3
     _hint =  "Try drawing the game tree.  How many moves (columns) are possible at each turn?"
     _solution = CS("num_leaves = 7*7*7")
+    
+class WhichMove(CodingProblem):
+    _var = "selected_move"
+    _hint = "For each potential move, how will the opponent respond?"
+    _solution = CS("selected_move = 3")
+    def check(self, ans):
+        try: 
+            move = int(ans)
+        except:
+            assert 1==0, "Your answer should be one of `1`, `2`, or `3`."
+        assert move in [1, 2, 3], "Your answer should be one of `1`, `2`, or `3`."
+        assert move == 3, "{} is incorrect.  Please try again.".format(move)
+        
+class Assumptions(ThoughtExperiment):
+    _hint = "hi"
+    _solution = "yo!"
 
 class JustSubmitEx3(CodingProblem):
-    _hint = "Follow the instructions to submit your agent to the competition."
-    _solution = "Follow the instructions to submit your agent to the competition."
-    _congrats = "Thank you for submitting your agent to the competition!"
+    _hint = "Follow the instructions to create an agent."
+    _solution = "Follow the instructions to create an agent."
+    _congrats = "Thank you for creating an agent!"
     _correct_message = ""
     def check(self):
         pass
@@ -28,6 +44,8 @@ class JustSubmitEx3(CodingProblem):
 qvars = bind_exercises(globals(), [
     WorseHeuristic,
     NumLeaves, 
+    WhichMove,
+    Assumptions,
     JustSubmitEx3
     ],
     var_format='q_{n}',
