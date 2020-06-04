d4dd06558287c655477ce9da9542f748d0261695
ryanholbrook <ryan@mathformachines.com>
2020-06-04 10:57:26 -0500

    Add tracking for lessons 1, 2, 3

--- notebooks/computer_vision/track_meta.py
+++ notebooks/computer_vision/track_meta.py
@@ -1,25 +1,43 @@
 # See also examples/example_track/track_meta.py for a longer, commented example
 track = dict(
     author_username='ryanholbrook',
-    course_name='computer_vision',
+    course_name='Computer Vision',
     course_url='https://www.kaggle.com/ryanholbrook/computer-vision'
 )
 
 lessons = [
-        dict(
-            # By convention, this should be a lowercase noun-phrase.
-            topic='Testing',
-            ),
+    {'topic': topic_name} for topic_name in
+    [
+        'The Convolutional Classifier',
+        'Convnet Architecture',
+        'Filter, Detect, Condense',
+        # 'Convolution and Pooling',
+        # 'Exploring Convnets',
+        # 'Transfer Learning',
+        # 'Data Augmentation',
+     ]
 ]
 
 notebooks = [
     dict(
-        filename='test.ipynb',
+        filename='tut1.ipynb',
         lesson_idx=0,
-        type='exercise',
-        scriptid=1,
-        ),
+        type='tutorial',
+    ),
+    dict(
+        filename='tut2.ipynb',
+        lesson_idx=1,
+        type='tutorial',
+    ),
+    dict(
+        filename='tut3.ipynb',
+        lesson_idx=2,
+        type='tutorial',
+    ),
 ]
 
 for nb in notebooks:
-    nb['dataset_sources'] = ["ryanholbrook/stanford-cars-for-learn"]
+    nb['dataset_sources'] = [
+        "ryanholbrook/stanford-cars-for-learn",
+        "ryanholbrook/saved-models",
+    ]
