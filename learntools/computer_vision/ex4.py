a245a8861eb35af77bb5387a7945f07af8cef017
ryanholbrook <ryan@mathformachines.com>
2020-07-06 08:38:47 -0500

    Add exercise 4 checking code

---
+++ learntools/computer_vision/ex4.py
@@ -0,0 +1,40 @@
+from learntools.core import *
+import tensorflow as tf
+
+
+class Q1(ThoughtExperiment):
+    _solution = ""
+
+
+class Q2(ThoughtExperiment):
+    _solution = ""
+
+
+class Q3A(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+class Q3B(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+class Q3C(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+Q3 = MultipartProblem(Q3A, Q3B, Q3C)
+
+
+class Q4(CodingProblem):
+    _hint = ""
+    _solution = ""
+    def check(self):
+        pass
+
+qvars = bind_exercises(globals(), [
+        Q1, Q2, Q3, Q4,
+    ],
+    var_format='q_{n}',
+)
+__all__ = list(qvars)
+    
