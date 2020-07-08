3ddbff7204c8458be777cdf194ce38170c1982d9
ryanholbrook <ryan@mathformachines.com>
2020-07-08 13:36:55 -0500

    Change to raw strings in exercise 4 checking

--- learntools/computer_vision/ex4.py
+++ learntools/computer_vision/ex4.py
@@ -12,12 +12,12 @@ class Q1B(ThoughtExperiment):
 Q1 = MultipartProblem(Q1A, Q1B)
 
 class Q2A(ThoughtExperiment):
-    _hint = "Stacking the second layer expanded the receptive field by one neuron on each side, giving $3+1+1=5$ for each dimension. If you expanded by one neuron again, what would you get?"
-    _solution = "The third layer would have a $7\times 7$ receptive field."
+    _hint = r"Stacking the second layer expanded the receptive field by one neuron on each side, giving $3+1+1=5$ for each dimension. If you expanded by one neuron again, what would you get?"
+    _solution = r"The third layer would have a $7 \times 7$ receptive field."
 
 class Q2B(ThoughtExperiment):
-    _hint = "This pooling layer collapses a $2\times 2$ patch into a single pixel, effectively *doubling* the number of connections along each dimension. "
-    _solution = "Doubling a $7 \times 7$ field produces a $14 \times 14$ field for the final outputs."
+    _hint = r"This pooling layer collapses a $2 \times 2$ patch into a single pixel, effectively *doubling* the number of connections along each dimension. "
+    _solution = r"Doubling a $7 \times 7$ field produces a $14 \times 14$ field for the final outputs."
 
 Q2 = MultipartProblem(Q2A, Q2B)
 
