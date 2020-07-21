a8ea99d3ffab279d5f48671f5feb05c67e6e0e72
Alexis Cook <alexis.cook@gmail.com>
2020-07-21 14:40:39 -0500

    revert one more chhange

--- learntools/data_viz_to_coder/ex7.py
+++ learntools/data_viz_to_coder/ex7.py
@@ -7,7 +7,7 @@ from learntools.core import *
 class AttachData(CodingProblem):
     def check(self):
         assert len(os.listdir('../input')) > 0, \
-        "Please save a CSV file and run this code cell again to get credit!"  
+        "Please attach a dataset and run this code cell again to get credit!"  
         
         # check if CSV file attached to notebook
         has_csv = False
