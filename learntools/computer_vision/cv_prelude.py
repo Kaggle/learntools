2df74c705fafaf2fd37a71ad4ce471c54f78079a
ryanholbrook <ryan@mathformachines.com>
2020-09-28 09:37:45 -0500

    Remove local compatibility

--- learntools/computer_vision/cv_prelude.py
+++ learntools/computer_vision/cv_prelude.py
@@ -1,15 +1,5 @@
 # This script accompanies Kaggle's Computer Vision course.
 
-
-# import os, sys
-# os.system("pip install -U -t /kaggle/working/ git+https://github.com/Kaggle/learntools.git@cv-course")
-# sys.path.append('kaggle/working')
-
-# import tensorflow as tf
-# config = tf.compat.v1.ConfigProto()
-# config.gpu_options.allow_growth = True
-# session = tf.compat.v1.InteractiveSession(config=config)
-
 # Imports
 import warnings
 import matplotlib.pyplot as plt
