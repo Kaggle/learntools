9e398a8c96ac4c4956dc19d5b4cff8f499c75fea
ryanholbrook <ryan@mathformachines.com>
2020-10-21 16:03:55 +0000

    Create checking code templates

---
+++ learntools/feature_engineering_new/ex6.py
@@ -0,0 +1,34 @@
+from learntools.core import *
+
+# ThoughtExperiment
+# EqualityCheckProblem
+# FunctionProblem
+# CodingProblem
+
+
+class Q1(ThoughtExperiment):
+    pass
+
+
+class Q2(ThoughtExperiment):
+    pass
+
+
+class Q3(ThoughtExperiment):
+    pass
+
+
+class Q4(ThoughtExperiment):
+    pass
+
+
+class Q5(ThoughtExperiment):
+    pass
+
+
+class Q6(ThoughtExperiment):
+    pass
+
+
+qvars = bind_exercises(globals(), [Q1, Q2, Q3, Q4, Q5, Q6], var_format="q_{n}")
+__all__ = list(qvars)
