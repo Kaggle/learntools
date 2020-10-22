d356a470bbe9d03a360bb2558a13a640c5825f7a
Ryan Holbrook <rholbrook@kaggle.com>
2020-10-22 18:56:00 +0000

    Add setup_data.sh

---
+++ notebooks/feature_engineering_new/setup_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Download the datasets used in the ML notebooks to correct relative_paths (../input/...)
+
+mkdir -p input
+
+DATASETS="ryanholbrook/fe-course-data"
+
+for slug in $DATASETS
+do
+    name=`echo $slug | cut -d '/' -f 2`
+    dest="input/$name"
+    mkdir -p $dest
+    kaggle d download -p $dest --unzip $slug
+done
