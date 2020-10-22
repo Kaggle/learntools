31f5e6d47f65fb44f72a009d19ae56b0f442b053
Ryan Holbrook <rholbrook@kaggle.com>
2020-10-22 20:04:56 +0000

    Recreate tutorials without code

--- notebooks/feature_engineering_new/track_meta.py
+++ notebooks/feature_engineering_new/track_meta.py
@@ -15,72 +15,72 @@ TOPICS = ["Transforming Features",                # 1
 lessons = [{'topic': topic_name} for topic_name in TOPICS]
 
 notebooks = [
-    # dict(
-    #     filename='tut1.ipynb',
-    #     lesson_idx=0,
-    #     type='tutorial',
-    #     scriptid=1,
-    #     ),
+    dict(
+        filename='tut1.ipynb',
+        lesson_idx=0,
+        type='tutorial',
+        scriptid=1,
+        ),
     dict(
         filename='ex1.ipynb',
         lesson_idx=0,
         type='exercise',
         scriptid=1,
         ),
-    # dict(
-    #     filename='tut2.ipynb',
-    #     lesson_idx=1,
-    #     type='tutorial',
-    #     scriptid=1,
-    #     ),
+    dict(
+        filename='tut2.ipynb',
+        lesson_idx=1,
+        type='tutorial',
+        scriptid=1,
+        ),
     dict(
         filename='ex2.ipynb',
         lesson_idx=1,
         type='exercise',
         scriptid=1,
         ),
-    # dict(
-    #     filename='tut3.ipynb',
-    #     lesson_idx=2,
-    #     type='tutorial',
-    #     scriptid=1,
-    #     ),
+    dict(
+        filename='tut3.ipynb',
+        lesson_idx=2,
+        type='tutorial',
+        scriptid=1,
+        ),
     dict(
         filename='ex3.ipynb',
         lesson_idx=2,
         type='exercise',
         scriptid=1,
         ),
-    # dict(
-    #     filename='tut4.ipynb',
-    #     lesson_idx=3,
-    #     type='tutorial',
-    #     scriptid=1,
-    #     ),
+    dict(
+        filename='tut4.ipynb',
+        lesson_idx=3,
+        type='tutorial',
+        scriptid=1,
+        ),
     dict(
         filename='ex4.ipynb',
         lesson_idx=3,
         type='exercise',
         scriptid=1,
         ),
-    # dict(
-    #     filename='tut5.ipynb',
-    #     lesson_idx=4,
-    #     type='tutorial',
-    #     scriptid=1,
-    #     ),
+    dict(
+        filename='tut5.ipynb',
+        lesson_idx=4,
+        type='tutorial',
+        scriptid=1,
+        ),
     dict(
         filename='ex5.ipynb',
         lesson_idx=4,
         type='exercise',
         scriptid=1,
         ),
-    # dict(
-    #     filename='tut6.ipynb',
-    #     lesson_idx=5,
-    #     type='tutorial',
-    #     scriptid=1,
-    #     ),
+    dict(
+        filename='tut6.ipynb',
+        lesson_idx=5,
+        type='tutorial',
+        scriptid=1,
+    ),
     dict(
         filename='ex6.ipynb',
         lesson_idx=5,
