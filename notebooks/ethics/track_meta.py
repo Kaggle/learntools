f4acd5901f1e94f8075a2a25c925c4e3bf4477fe
Alexis Cook <alexis.cook@gmail.com>
2020-12-23 15:09:14 -0500

    setting up track_meta

--- notebooks/ethics/track_meta.py
+++ notebooks/ethics/track_meta.py
@@ -6,23 +6,72 @@ track = dict(
     course_forum_url='https://www.kaggle.com/learn-forum',
 )
 
-TOPICS = [
-    'Bias',
-]
+lessons = [ {'topic': topic_name} for topic_name in
+                    ["Introduction to AI Ethics",
+                     "Human-Centered Design",
+                     "Identifying Bias",
+                     "Fairness",
+                     "Model Cards"
+                     ]
+            ]
 
 lessons = [{'topic': topic_name} for topic_name in TOPICS]
 
 notebooks = [
     dict(
-        filename='tut.ipynb',
+        filename='tut1.ipynb',
         lesson_idx=0,
         type='tutorial',
         ),
     dict(
-        filename='ex.ipynb',
+        filename='ex1.ipynb',
         lesson_idx=0,
         type='exercise',
-        scriptid=1,
+        scriptid=-1
+        ),
+    dict(
+        filename='tut2.ipynb',
+        lesson_idx=1,
+        type='tutorial',
+        ),
+    dict(
+        filename='ex2.ipynb',
+        lesson_idx=1,
+        type='exercise',
+        scriptid=-1
+        ),
+    dict(
+        filename='tut3.ipynb',
+        lesson_idx=2,
+        type='tutorial',
+        ),
+    dict(
+        filename='ex3.ipynb',
+        lesson_idx=2,
+        type='exercise',
+        scriptid=-1,
         competition_sources='jigsaw-unintended-bias-in-toxicity-classification'
+        ),
+    dict(
+        filename='tut4.ipynb',
+        lesson_idx=3,
+        type='tutorial',
+        ),
+    dict(
+        filename='ex4.ipynb',
+        lesson_idx=3,
+        type='exercise',
+        scriptid=-1
+        ),
+    dict(
+        filename='tut5.ipynb',
+        lesson_idx=4,
+        type='tutorial',
+        ),
+    dict(
+        filename='ex5.ipynb',
+        lesson_idx=4,
+        type='exercise',
+        scriptid=-1
         ),
 ]
