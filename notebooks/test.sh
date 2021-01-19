d2d8ba41df4dabc931ac237f8bc3acf51d3b1e4f
Alexis Cook <alexis.cook@gmail.com>
2021-01-19 09:21:09 -0500

    reorder test

--- notebooks/test.sh
+++ notebooks/test.sh
@@ -5,7 +5,7 @@ set -e
 
 # Filter by tracks if first argument set.
 TRACKS="computer_vision deep_learning_intro pandas python machine_learning sql data_viz_to_coder ml_intermediate sql_advanced feature_engineering geospatial nlp game_ai data_cleaning"
-TESTABLE_NOTEBOOK_TRACKS="computer_vision deep_learning_intro geospatial python pandas machine_learning data_viz_to_coder ml_intermediate nlp feature_engineering game_ai data_cleaning"
+TESTABLE_NOTEBOOK_TRACKS="data_viz_to_coder ml_intermediate nlp feature_engineering game_ai data_cleaning computer_vision deep_learning_intro geospatial python pandas machine_learning"
 if [[ -n $1 && $1 != "all" ]]; then
     TRACKS=$1
     TESTABLE_NOTEBOOK_TRACKS=$1
