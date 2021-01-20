05422bb66cb5420bbd28904f5d5575ef840cdba5
Ryan Holbrook <rholbrook@kaggle.com>
2021-01-20 22:47:25 +0000

    Add bonus lesson

--- notebooks/feature_engineering_new/track_meta.py
+++ notebooks/feature_engineering_new/track_meta.py
@@ -12,24 +12,34 @@ TOPICS = [
     "Clustering with k-Means",  # 4
     "Principal Components Analysis",  # 5
     "Target Encoding",  # 6
+    "Getting Started with House Prices",  # Bonus
 ]
 lessons = [{"topic": topic_name} for topic_name in TOPICS]
 
 notebooks = [
-    dict(filename="tut1.ipynb", lesson_idx=0, type="tutorial", scriptid=1,),
-    dict(filename="ex1.ipynb", lesson_idx=0, type="exercise", scriptid=1,),
-    dict(filename="tut2.ipynb", lesson_idx=1, type="tutorial", scriptid=1,),
-    dict(filename="ex2.ipynb", lesson_idx=1, type="exercise", scriptid=1,),
-    dict(filename="tut3.ipynb", lesson_idx=2, type="tutorial", scriptid=1,),
-    dict(filename="ex3.ipynb", lesson_idx=2, type="exercise", scriptid=1,),
-    dict(filename="tut4.ipynb", lesson_idx=3, type="tutorial", scriptid=1,),
-    dict(filename="ex4.ipynb", lesson_idx=3, type="exercise", scriptid=1,),
-    dict(filename="tut5.ipynb", lesson_idx=4, type="tutorial", scriptid=1,),
-    dict(filename="ex5.ipynb", lesson_idx=4, type="exercise", scriptid=1,),
-    dict(filename="tut6.ipynb", lesson_idx=5, type="tutorial", scriptid=1,),
-    dict(filename="ex6.ipynb", lesson_idx=5, type="exercise", scriptid=1,),
+    dict(filename="tut1.ipynb", lesson_idx=0, type="tutorial", scriptid=1),
+    dict(filename="ex1.ipynb", lesson_idx=0, type="exercise", scriptid=1),
+    dict(filename="tut2.ipynb", lesson_idx=1, type="tutorial", scriptid=1),
+    dict(filename="ex2.ipynb", lesson_idx=1, type="exercise", scriptid=1),
+    dict(filename="tut3.ipynb", lesson_idx=2, type="tutorial", scriptid=1),
+    dict(filename="ex3.ipynb", lesson_idx=2, type="exercise", scriptid=1),
+    dict(filename="tut4.ipynb", lesson_idx=3, type="tutorial", scriptid=1),
+    dict(filename="ex4.ipynb", lesson_idx=3, type="exercise", scriptid=1),
+    dict(filename="tut5.ipynb", lesson_idx=4, type="tutorial", scriptid=1),
+    dict(filename="ex5.ipynb", lesson_idx=4, type="exercise", scriptid=1),
+    dict(filename="tut6.ipynb", lesson_idx=5, type="tutorial", scriptid=1),
+    dict(filename="ex6.ipynb", lesson_idx=5, type="exercise", scriptid=1),
 ]
-
-
 for nb in notebooks:
     nb["dataset_sources"] = ["ryanholbrook/fe-course-data"]
+
+notebooks.append(
+    dict(
+        filename="tut_bonus.ipynb",
+        lesson_idx=6,
+        type="tutorial",
+        scriptid=1,
+        dataset_sources=["ryanholbrook/fe-course-data"],
+        competition_sources=["house-prices-advanced-regression-techniques"],
+    )
+)
