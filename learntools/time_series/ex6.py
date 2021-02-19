5a6da64862b0e37d3febb1b4c08b04e123fd42aa
Ryan Holbrook <rholbrook@kaggle.com>
2021-02-19 17:02:20 +0000

    Initial commit

---
+++ learntools/time_series/ex6.py
@@ -0,0 +1,30 @@
+from learntools.core import *
+
+
+class Q1(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+
+class Q2(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+
+class Q3(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+
+class Q4(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+
+class Q5(ThoughtExperiment):
+    _hint = ""
+    _solution = ""
+
+
+qvars = bind_exercises(globals(), [Q1, Q2, Q3, Q4, Q5], var_format="q_{n}")
+__all__ = list(qvars)
