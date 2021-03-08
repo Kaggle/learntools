79e9d5965aefbb8643b1d08b9b2d079a8e4a99bd
Alexis Cook <alexis.cook@gmail.com>
2021-03-08 14:57:07 -0500

    hcd lesson changes

--- learntools/ethics/ex2.py
+++ learntools/ethics/ex2.py
@@ -13,7 +13,10 @@ class DetectCancer(ThoughtExperiment):
     _correct_message = ("Yes, it would. People would generally agree that the goal is desirable, especially "
                         "since the AI system will be working with pathologists rather than in their place. AI "
                         "can help people with repetitive tasks and AI systems have proven effective in similar "
-                        "medical image recognition use cases.")
+                        "medical image recognition use cases. That said, it is important to follow current "
+                        "industry best practices and to be thorough in the rest of the design process, including "
+                        "in analyzing harms and in considering how medical practitioners will actually interact "
+                        "with the product in a medical setting.")
     
 class FlagActivity(ThoughtExperiment):
     _hint = ""
@@ -30,7 +33,10 @@ class PrototypeChatbot(ThoughtExperiment):
     _solution = ""
     _congrats = "Solution"
     _correct_message = ("The correct answer is: Build a non-AI prototype quickly and start testing it with a "
-                        "diverse group of potential users.")
+                        "diverse group of potential users. Iterating on a non-AI prototype is easier, faster "
+                        "and less expensive than iterating on an AI prototype. Iterating on a non-AI prototype "
+                        "also provides early information on user expectations, interactions and needs. This "
+                        "information should inform the eventual design of AI prototypes.")
 
     
 class DetectMisinformation(ThoughtExperiment):
