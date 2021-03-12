1edb16784ba4aa220687a61ee6e544355d4a3eed
Alexis Cook <alexis.cook@gmail.com>
2021-03-12 17:56:13 -0500

    adding ethics changes

--- notebooks/ethics/track_meta.py
+++ notebooks/ethics/track_meta.py
@@ -1,6 +1,6 @@
 # See also examples/example_track/track_meta.py for a longer, commented example
 track = dict(
-    author_username='alexisbcook',
+    author_username='var0101',
     course_name='AI Ethics',
     course_url='https://www.kaggle.com/learn/ai-ethics',
     course_forum_url='https://www.kaggle.com/learn-forum',
@@ -36,25 +36,29 @@ notebooks = [
         filename='tut3.ipynb',
         lesson_idx=2,
         type='tutorial',
+        author='alexisbcook'
         ),
     dict(
         filename='ex3.ipynb',
         lesson_idx=2,
         type='exercise',
         scriptid=-1,
-        competition_sources="jigsaw-unintended-bias-in-toxicity-classification"
+        competition_sources="jigsaw-unintended-bias-in-toxicity-classification",
+        author='alexisbcook'
         ),
     dict(
         filename='tut4.ipynb',
         lesson_idx=3,
         type='tutorial',
+        author='alexisbcook'
         ),
     dict(
         filename='ex4.ipynb',
         lesson_idx=3,
         type='exercise',
-        scriptid=-10,
-        dataset_sources="alexisbcook/synthetic-credit-card-approval"
+        scriptid=-1,
+        dataset_sources="alexisbcook/synthetic-credit-card-approval",
+        author='alexisbcook'
         ),
     dict(
         filename='tut5.ipynb',
