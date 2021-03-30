a111059df7a4864d6e0c5b7496c82ce248194181
Vincent Roseberry <rosbo@google.com>
2021-03-30 02:25:56 +0000

    Update checks for data_viz_to_coder track.
    The new Python image has matplotlib 3.4 which includes a slightly
    different API for Axes management: https://github.com/matplotlib/matplotlib/releases/tag/v3.4.0

--- learntools/data_viz_to_coder/ex4.py
+++ learntools/data_viz_to_coder/ex4.py
@@ -52,7 +52,7 @@ sns.scatterplot(x=candy_data['sugarpercent'], y=candy_data['winpercent'])
     def check(self, passed_plt):
         assert len(passed_plt.figure(1).axes) > 0, "Please write code to create a scatter plot."
         
-        children = passed_plt.axes().get_children()
+        children = passed_plt.gca().get_children()
         
         assert all(isinstance(x, matplotlib.spines.Spine) for x in children[1:5]), \
         "Is your figure a scatter plot? Please use `sns.scatterplot` to generate your figure."
@@ -84,7 +84,7 @@ sns.regplot(x=candy_data['sugarpercent'], y=candy_data['winpercent'])
         assert len(passed_plt.figure(1).axes) > 0, \
         "Please write code to create a scatter plot with a regression line."
         
-        children = passed_plt.axes().get_children()
+        children = passed_plt.gca().get_children()
         
         assert all(isinstance(x, matplotlib.spines.Spine) for x in children[3:7]), \
         ("Is your figure a scatter plot with a regression line? "
@@ -176,7 +176,7 @@ sns.swarmplot(x=candy_data['chocolate'], y=candy_data['winpercent'])
     def check(self, passed_plt):
         assert len(passed_plt.figure(1).axes) > 0, "Please write code to create a categorical scatter plot."
         
-        children = passed_plt.axes().get_children()
+        children = passed_plt.gca().get_children()
         
         assert all(isinstance(x, matplotlib.spines.Spine) for x in children[2:6]), \
         "Is your figure a categorical scatter plot?  Please use `sns.swarmplot` to generate your figure."
