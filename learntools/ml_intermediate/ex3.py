0701aafdfd6e987b35cadc4b604c93595e13dd0b
Alexis Cook <alexis.cook@gmail.com>
2021-06-30 11:37:53 -0400

    revert variable name change

--- learntools/ml_intermediate/ex3.py
+++ learntools/ml_intermediate/ex3.py
@@ -46,41 +46,41 @@ class LabelA(ThoughtExperiment):
                  "scikit-learn, the code will throw an error.")
 
 class LabelB(CodingProblem):
-    _vars = ['ordinal_X_train', 'ordinal_X_valid']
+    _vars = ['label_X_train', 'label_X_valid']
     _hint = ("Use the `OrdinalEncoder` class from scikit-learn. You should only encode the columns in "
              "`good_label_cols`.")
     _solution = CS(
 """# Drop categorical columns that will not be encoded
-ordinal_X_train = X_train.drop(bad_label_cols, axis=1)
-ordinal_X_valid = X_valid.drop(bad_label_cols, axis=1)
+label_X_train = X_train.drop(bad_label_cols, axis=1)
+label_X_valid = X_valid.drop(bad_label_cols, axis=1)
 
 # Apply ordinal encoder
 ordinal_encoder = OrdinalEncoder()
-ordinal_X_train[good_label_cols] = ordinal_encoder.fit_transform(X_train[good_label_cols])
-ordinal_X_valid[good_label_cols] = ordinal_encoder.transform(X_valid[good_label_cols])
+label_X_train[good_label_cols] = ordinal_encoder.fit_transform(X_train[good_label_cols])
+label_X_valid[good_label_cols] = ordinal_encoder.transform(X_valid[good_label_cols])
 """)
 
-    def check(self, ordinal_X_train, ordinal_X_valid):
+    def check(self, label_X_train, label_X_valid):
 
-        assert type(ordinal_X_train) == pd.core.frame.DataFrame, \
-        "`ordinal_X_train` is not a pandas DataFrame."
+        assert type(label_X_train) == pd.core.frame.DataFrame, \
+        "`label_X_train` is not a pandas DataFrame."
 
-        assert type(ordinal_X_valid) == pd.core.frame.DataFrame, \
-        "`ordinal_X_valid` is not a pandas DataFrame."
+        assert type(label_X_valid) == pd.core.frame.DataFrame, \
+        "`label_X_valid` is not a pandas DataFrame."
 
-        assert not(any((ordinal_X_train.dtypes == 'object').values)), \
+        assert not(any((label_X_train.dtypes == 'object').values)), \
         "You still need to encode some of the categorical columns in your training data."
 
-        assert not(any((ordinal_X_valid.dtypes == 'object').values)), \
+        assert not(any((label_X_valid.dtypes == 'object').values)), \
         "You still need to encode some of the categorical columns in your validation data."
         
         # remove 45 after nb update
-        assert ordinal_X_train.shape[1] in [57, 45], \
-        "`ordinal_X_train` does not have the correct number of columns."
+        assert label_X_train.shape[1] in [57, 45], \
+        "`label_X_train` does not have the correct number of columns."
 
         # remove 45 after nb update
-        assert ordinal_X_valid.shape[1] in [57, 45], \
-        "`ordinal_X_valid` does not have the correct number of columns."
+        assert label_X_valid.shape[1] in [57, 45], \
+        "`label_X_valid` does not have the correct number of columns."
 
 Label = MultipartProblem(LabelA, LabelB)
 
@@ -99,7 +99,7 @@ num_cols_neighborhood = 25
 """)
 
 class CardinalityB(EqualityCheckProblem):
-    _vars = ['OH_entries_added', 'ordinal_entries_added']
+    _vars = ['OH_entries_added', 'label_entries_added']
     _expected = [990000, 0]
     _hint = ("To calculate how many entries are added to the dataset through the one-hot encoding, "
              "begin by calculating how many entries are needed to encode the categorical variable "
@@ -113,7 +113,7 @@ OH_entries_added = 1e4*100 - 1e4
 
 # How many entries are added to the dataset by
 # replacing the column with an ordinal encoding?
-ordinal_entries_added = 0
+label_entries_added = 0
 """)
 
 Cardinality = MultipartProblem(CardinalityA, CardinalityB)
