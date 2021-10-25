9cb3fde6ed5198c58351dcb3a387f97a276b814c
Alexis Cook <alexis.cook@gmail.com>
2021-10-25 16:45:40 -0400

    add new object class

--- learntools/geospatial/tools.py
+++ learntools/geospatial/tools.py
@@ -7,6 +7,10 @@ all_addresses = ['2224 Shattuck Avenue Berkeley CA',
                  '3001 Telegraph Avenue Berkeley CA',
                  '2128 Oxford Street Berkeley CA']
 
+class Object:
+    def __init__(self, point):
+        self.point = point
+
 class Nominatim:
     def __init__(self, user_agent):
         self.user_agent = user_agent
@@ -14,6 +18,8 @@ class Nominatim:
         try:
             my_index = all_addresses.index(address)
             gdf = gpd.read_file("../input/geospatial-learn-course-data/ex4_files/ex4_files/add_{}.shp".format(my_index))
-            return Point(latitude=gdf.geometry.y[0], longitude=gdf.geometry.x[0])
+            a = Object(point=Point(latitude=gdf.geometry.y[0], longitude=gdf.geometry.x[0]))
+            return a
         except:
             return None
+        
