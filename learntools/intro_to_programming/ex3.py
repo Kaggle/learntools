9b58183e936be3c75330c0ce63094b5353aaff52
Alexis Cook <alexis.cook@gmail.com>
2022-03-09 15:55:02 -0500

    work on ex3

--- learntools/intro_to_programming/ex3.py
+++ learntools/intro_to_programming/ex3.py
@@ -1,5 +1,9 @@
 from learntools.core import *
 
+def get_expected_cost(beds, baths, has_basement):
+    value = 80000 + 30000 * beds + 10000 * baths + 40000 * has_basement
+    return value
+
 class FloatToInt(ThoughtExperiment):
     _solution = ("Negative floats are always rounded UP to the closest integer (for instance, "
                  "both -1.1 and -1.9 are rounded up to -1). Positive floats are rounded either "
@@ -19,53 +23,36 @@ class MultiplyBooleans(ThoughtExperiment):
 class EstimateHouseValueBool(FunctionProblem):
     _var = 'get_expected_cost'
     _test_cases = [
-        ((0, 0), 80000),
-        ((0, 1), 90000),
-        ((1, 0), 110000),
-        ((1, 1), 120000),
-        ((1, 2), 130000),
-        ((2, 3), 170000),
-        ((3, 2), 190000),
-        ((3, 3), 200000),
-        ((3, 4), 210000),
+        ((1, 1, False), 120000),
+        ((2, 1, True), 190000),
+        ((3, 2, True), 230000),
+        ((4, 5, False), 250000),
     ]
-    _hint = "HINT: If you're ever stuck on a question, it's a good idea to look at the hint before viewing the solution."
-    _solution = ("SOLUTION: If you're still stuck on a question after viewing the hint and re-reading the tutorial, "
-                 "you can view the solution.  You can also view the solution after you have successfully submitted "
-                 "your own answer, to check if the official solution is any different (there may be more than "
-                 "one right answer!).")
-
-class AddingBooleans(EqualityCheckProblem):
-    _vars = ['births_per_min', 'births_per_day']
-    _expected = [250, births_per_min * mins_per_hour * hours_per_day]
-    _hint = ("How can you use the variables to calculate the number of minutes in one day?  Once you have that, you "
-             "need only multiply that number by the number of births per minute.")
+    _hint = ("The variable `has_basement` is either `True` or `False`.  What happens when you "
+             "multiply it by 40000 (the value of a basement)?  Refer to the previous question "
+             "if you are unsure.")
     _solution = CS(
-"""# Set the value of the births_per_min variable
-births_per_min = 250
-
-# Set the value of the births_per_day variable
-births_per_day = births_per_min * mins_per_hour * hours_per_day
+"""def get_expected_cost(beds, baths, has_basement):
+    value = 80000 + 30000 * beds + 10000 * baths + 40000 * has_basement
+    return value
 """)
+
+class AddingBooleans(ThoughtExperiment):
+    _solution = "When you add booleans, adding `False` is equivalent to adding 0, and adding `True` is equivalent to adding 1."
     
-class CustomEngravings(EqualityCheckProblem):
-    _vars = ['survived_fraction', 'minors_fraction']
-    _expected = [survived/total, minors/total]
-    _hint = ("To get the fraction of people who survived the titanic, you need to divide the total number of people by the "
-             "number of people who surivived.  Remember the variables that you can use to answer this question: `survived`, "
-             "`total`, and `minors`.")
+class CustomEngravings(FunctionProblem):
+    _var = 'cost_of_project'
+    _test_cases = [
+        (("Charlie+Denver", True), 240),
+        (("08/10/2000", False), 120),
+        ((3, 2, True), 230000),
+        ((4, 5, False), 250000),
+    ]
+    _hint = ("There are two options - either the project uses solid gold or does not.  With this in mind, you can structure your solution like this: `cost = solid_gold * ____ + (not_solid_gold) * ____`.  You need to figure out how to fill in the blanks.")
     _solution = CS(
-"""# Fill in the value of the survived_fraction variable 
-survived_fraction = survived/total
-
-# Print the value of the survived_fraction variable
-print(survived_fraction)
-
-# Fill in the value of the minors_fraction variable
-minors_fraction = minors/total 
-
-# Print the value of the minors_fraction variable
-print(minors_fraction)
+"""def cost_of_project(engraving, solid_gold):
+    cost = solid_gold * (100 + 10 * len(engraving)) + (not solid_gold) * (50 + 7 * len(engraving))
+    return cost
 """)
 
 
