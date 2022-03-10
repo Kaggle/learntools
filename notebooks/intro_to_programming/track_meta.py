a4471474b14f88274683c6daf900fb57bda7bc38
Alexis Cook <alexis.cook@gmail.com>
2022-03-10 11:04:21 -0500

    adding prod, scriptids

--- notebooks/intro_to_programming/track_meta.py
+++ notebooks/intro_to_programming/track_meta.py
@@ -17,11 +17,11 @@ lessons = [{"topic": topic_name} for topic_name in TOPICS]
 
 notebooks = [
     dict(filename="tut1.ipynb", lesson_idx=0, type="tutorial"),
-    dict(filename="ex1.ipynb", lesson_idx=0, type="exercise", scriptid=-1),
+    dict(filename="ex1.ipynb", lesson_idx=0, type="exercise", scriptid=25258219),
     dict(filename="tut2.ipynb", lesson_idx=1, type="tutorial"),
-    dict(filename="ex2.ipynb", lesson_idx=1, type="exercise", scriptid=-1),
+    dict(filename="ex2.ipynb", lesson_idx=1, type="exercise", scriptid=25258227),
     dict(filename="tut3.ipynb", lesson_idx=2, type="tutorial"),
-    dict(filename="ex3.ipynb", lesson_idx=2, type="exercise", scriptid=-1),
+    dict(filename="ex3.ipynb", lesson_idx=2, type="exercise", scriptid=25258226),
     #dict(filename="tut4.ipynb", lesson_idx=3, type="tutorial"),
     #dict(filename="ex4.ipynb", lesson_idx=3, type="exercise", scriptid=-1),
     #dict(filename="tut5.ipynb", lesson_idx=4, type="tutorial"),
