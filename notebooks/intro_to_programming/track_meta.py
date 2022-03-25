75bcafcff61d5b5c6dfb67bb6ddf21bc745353a5
Alexis Cook <alexis.cook@gmail.com>
2022-03-25 11:28:04 -0400

    add lesson 5, still need edit q3

--- notebooks/intro_to_programming/track_meta.py
+++ notebooks/intro_to_programming/track_meta.py
@@ -10,7 +10,7 @@ TOPICS = [
     "Functions",  # 2
     "Data Types",  # 3
     "Conditions and Conditional Statements",  # 4
-    #"Lists",  # 5
+    "Lists",  # 5
     #"Loops",  # 6
 ]
 lessons = [{"topic": topic_name} for topic_name in TOPICS]
@@ -24,8 +24,8 @@ notebooks = [
     dict(filename="ex3.ipynb", lesson_idx=2, type="exercise", scriptid=25258226),
     dict(filename="tut4.ipynb", lesson_idx=3, type="tutorial"),
     dict(filename="ex4.ipynb", lesson_idx=3, type="exercise", scriptid=-1),
-    #dict(filename="tut5.ipynb", lesson_idx=4, type="tutorial"),
-    #dict(filename="ex5.ipynb", lesson_idx=4, type="exercise", scriptid=-1),
+    dict(filename="tut5.ipynb", lesson_idx=4, type="tutorial"),
+    dict(filename="ex5.ipynb", lesson_idx=4, type="exercise", scriptid=-1),
     #dict(filename="tut6.ipynb", lesson_idx=5, type="tutorial"),
     #dict(filename="ex6.ipynb", lesson_idx=5, type="exercise", scriptid=-1),
 ]
