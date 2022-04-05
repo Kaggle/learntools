9c353a7b9892d70988a50e281daf1301b713e9e3
Alexis Cook <alexis.cook@gmail.com>
2022-04-05 17:01:46 -0400

    correct script ids

--- notebooks/intro_to_programming/track_meta.py
+++ notebooks/intro_to_programming/track_meta.py
@@ -15,16 +15,16 @@ TOPICS = [
 lessons = [{"topic": topic_name} for topic_name in TOPICS]
 
 notebooks = [
-    dict(filename="tut1.ipynb", lesson_idx=0, type="tutorial"),
+    dict(filename="tut1.ipynb", lesson_idx=0, type="tutorial"),      # 25258229
     dict(filename="ex1.ipynb", lesson_idx=0, type="exercise", scriptid=25258219),
-    dict(filename="tut2.ipynb", lesson_idx=1, type="tutorial"),
+    dict(filename="tut2.ipynb", lesson_idx=1, type="tutorial"),      # 25258223
     dict(filename="ex2.ipynb", lesson_idx=1, type="exercise", scriptid=25258227),
-    dict(filename="tut3.ipynb", lesson_idx=2, type="tutorial"),
+    dict(filename="tut3.ipynb", lesson_idx=2, type="tutorial"),      # 25258222
     dict(filename="ex3.ipynb", lesson_idx=2, type="exercise", scriptid=25258226),
-    dict(filename="tut4.ipynb", lesson_idx=3, type="tutorial"),
+    dict(filename="tut4.ipynb", lesson_idx=3, type="tutorial"),      # 25880617
     dict(filename="ex4.ipynb", lesson_idx=3, type="exercise", scriptid=25880622),
-    dict(filename="tut5.ipynb", lesson_idx=4, type="tutorial"),
-    dict(filename="ex5.ipynb", lesson_idx=4, type="exercise", scriptid=25880656),
+    dict(filename="tut5.ipynb", lesson_idx=4, type="tutorial"),      # 26034860
+    dict(filename="ex5.ipynb", lesson_idx=4, type="exercise", scriptid=26034864),
 ]
 
 for nb in notebooks:
