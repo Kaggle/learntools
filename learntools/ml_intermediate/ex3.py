1590f916b512ddc0acd08899ea0d62016aeb9535
Alexis Cook <alexiscook@google.com>
2023-04-14 15:57:52 -0400

    ronehot encoding no longer removes column names

--- learntools/ml_intermediate/ex3.py
+++ learntools/ml_intermediate/ex3.py
@@ -129,10 +129,6 @@ OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
 OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[low_cardinality_cols]))
 OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[low_cardinality_cols]))
 
-# One-hot encoding removed index; put it back
-OH_cols_train.index = X_train.index
-OH_cols_valid.index = X_valid.index
-
 # Remove categorical columns (will replace with one-hot encoding)
 num_X_train = X_train.drop(object_cols, axis=1)
 num_X_valid = X_valid.drop(object_cols, axis=1)
@@ -140,7 +136,6 @@ num_X_valid = X_valid.drop(object_cols, axis=1)
 # Add one-hot encoded columns to numerical features
 OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
 OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
-
 """)
 
     def check(self, OH_X_train, OH_X_valid):
