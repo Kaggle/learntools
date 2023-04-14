03bb3630d6fe459cdc2bb03638f6a204d30bc304
Alexis Cook <alexiscook@google.com>
2023-04-14 17:17:14 -0400

    tut3, ex3 changes to ml_intermediate

--- learntools/ml_intermediate/ex3.py
+++ learntools/ml_intermediate/ex3.py
@@ -129,6 +129,10 @@ OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
 OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[low_cardinality_cols]))
 OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[low_cardinality_cols]))
 
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
 # Remove categorical columns (will replace with one-hot encoding)
 num_X_train = X_train.drop(object_cols, axis=1)
 num_X_valid = X_valid.drop(object_cols, axis=1)
