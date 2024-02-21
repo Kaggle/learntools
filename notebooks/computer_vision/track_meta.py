9c70809b7438b1837d75f0535f6f8b690004c20b
Jim Plotts <jplotts@google.com>
2024-02-21 16:16:14 +0000

    Enable kernels to be pinned to the original docker image
    Also applies original pinning type to the Computer Vision and Deep
    Learning tracks, which would be broken by the Keras 3 upgrade. These
    have already been pinned via direct DB update, but this change is needed
    to make sure that they don't revert back to latest during the next push.
    
    http://b/324889848

--- notebooks/computer_vision/track_meta.py
+++ notebooks/computer_vision/track_meta.py
@@ -24,73 +24,85 @@ notebooks = [
         lesson_idx=0,
         type='tutorial',
         enable_gpu=True,
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="ex1.ipynb",
         lesson_idx=0,
         type='exercise',
         enable_gpu=True,
-        scriptid=10781907
+        scriptid=10781907,
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="tut2.ipynb",
         lesson_idx=1,
         type='tutorial',
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="ex2.ipynb",
         lesson_idx=1,
         type='exercise',
-        scriptid=11989557
+        scriptid=11989557,
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="tut3.ipynb",
         lesson_idx=2,
         type='tutorial',
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="ex3.ipynb",
         lesson_idx=2,
         type='exercise',
-        scriptid=11989559
+        scriptid=11989559,
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="tut4.ipynb",
         lesson_idx=3,
         type='tutorial',
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="ex4.ipynb",
         lesson_idx=3,
         type='exercise',
-        scriptid=12400209
+        scriptid=12400209,
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="tut5.ipynb",
         lesson_idx=4,
         type='tutorial',
         enable_gpu=True,
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="ex5.ipynb",
         lesson_idx=4,
         type='exercise',
         enable_gpu=True,
-        scriptid=11989565
+        scriptid=11989565,
+        docker_image_pinning_type="original",
     ),
     dict(
         filename="tut6.ipynb",
         lesson_idx=5,
         type='tutorial',
         enable_gpu=True,
+        docker_image_pinning_type="original",
     ), 
     dict(
         filename="ex6.ipynb",
         lesson_idx=5,
         type='exercise',
         enable_gpu=True,
-        scriptid=11991328
-    ),  
+        scriptid=11991328,
+        docker_image_pinning_type="original",
+    ),
 ]
 
 for nb in notebooks:
