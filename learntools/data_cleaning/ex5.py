da2b0b311a0f3738e0978f1c8787bce1ebf18866
Marco Rampin <71671995+marcorampin@users.noreply.github.com>
2024-10-11 00:10:42 +0200

    ex5.py typo correction
    Corrected a typo in a comment describing the matches min_ratio

--- learntools/data_cleaning/ex5.py
+++ learntools/data_cleaning/ex5.py
@@ -31,7 +31,7 @@ def replace_matches_in_column(df, column, string_to_match, min_ratio = 47):
     matches = fuzzywuzzy.process.extract(string_to_match, strings, 
                                          limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio)
 
-    # only get matches with a ratio > 90
+    # only get matches with a ratio > 47
     close_matches = [matches[0] for matches in matches if matches[1] >= min_ratio]
 
     # get the rows of all the close matches in our dataframe
