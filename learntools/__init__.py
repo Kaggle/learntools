7c0888ba90a8e44542db3e834790ad936e1a0984
Jim Plotts <jplotts@google.com>
2025-03-27 10:08:09 -0400

    Rough implementation using kagglesdk
    This shouldn't impact any existing users, because it depends on an environment variable being set. This will enable us to rollout gradually when we're ready.  For now, there are still a few things to be done, primarily improving kagglesdk authorization.
    
    http://b/379083750

--- learntools/__init__.py
+++ learntools/__init__.py
@@ -2,4 +2,4 @@ from . import core, data_viz_to_coder, deep_learning, embeddings, gans, \
               machine_learning, ml_explainability, ml_insights, ml_intermediate, python, \
               sql
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
